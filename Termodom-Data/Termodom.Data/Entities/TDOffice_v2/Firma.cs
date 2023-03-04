bdccbae54e8c4127e60efee9e37ef3353e7c4024
Aleksa Ristic <aristiccitsira@gmail.com>
2023-03-04 11:55:18 +0100

    stage

---
+++ Termodom-Data/Termodom.Data/Entities/TDOffice_v2/Firma.cs
@@ -0,0 +1,15 @@
+﻿namespace Termodom.Data.Entities.TDOffice_v2
+{
+    public class Firma
+    {
+        public int ID { get; set; }
+        public string Naziv { get; set; }
+        public string TR { get; set; }
+        public string PIB { get; set; }
+        public string MB { get; set; }
+        public string Adresa { get; set; }
+        public string Grad { get; set; }
+        public int PPID { get; set; }
+        public int GlavniMagacin { get; set; }
+    }
+}
---
+++ Termodom-Data/Termodom.Data/Entities/TDOffice_v2/FirmaDictionary.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Termodom.Data.Entities.TDOffice_v2
+{
+    public class FirmaDictionary : ReadOnlyDictionary<int, Firma>
+    {
+        public FirmaDictionary(IDictionary<int, Firma> dictionary) : base(dictionary)
+        {
+        }
+    }
+}
