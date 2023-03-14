4dbf44dbaf194bcc228f9997d2e38012e9788c2e
Aleksa Ristic <32262570+DoLoop216@users.noreply.github.com>
2023-03-14 22:26:22 +0100

    Added extension for handling TDBrain responses

---
+++ src/Termodom-TDOffice-v2/TDOffice_v2/Komercijalno/TarifeManager.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Termodom.Data.Entities.Komercijalno;
+
+namespace TDOffice_v2.Komercijalno
+{
+    public static class TarifeManager
+    {
+        public static async Task<TarifaDictionary> DictionaryAsync()
+        {
+            var response = await TDBrain_v3.GetAsync($"/komercijalno/banka/dictionary");
+
+            switch ((int)response.StatusCode)
+            {
+                case 200:
+                    return JsonConvert.DeserializeObject<TarifaDictionary>(await response.Content.ReadAsStringAsync());
+                case 500:
+                    throw new Termodom.Data.Exceptions.APIServerException();
+                default:
+                    throw new Termodom.Data.Exceptions.APIUnhandledStatusException(response.StatusCode);
+            }
+        }
+    }
+}
