ba26cddf0554270c451d87006181d4d6d5d3f98a
Aleksa Ristic <32262570+DoLoop216@users.noreply.github.com>
2023-03-27 14:13:59 +0200

    Initial DBMigrations project

---
+++ src/DBMigrations/DBMigrations/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace DBMigrations
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; private set; }
+
+        public Startup(IWebHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(provider => Configuration);
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+#if DEBUG
+            app.UseDeveloperExceptionPage();
+#endif
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
