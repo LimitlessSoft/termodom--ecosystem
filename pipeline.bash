96eb7cbf0010e602b078e2c2075eaa2fb1140cf7
Aleksa Ristic <32262570+DoLoop216@users.noreply.github.com>
2023-06-17 23:40:05 +0200

    Stage

---
+++ pipeline.bash
@@ -0,0 +1,4 @@
+docker stop termodom--td-brain
+docker rm termodom--td-brain
+docker build . -t limitlesssoft/termodom--td-brain:%build.counter%
+docker run -p 32775:80 --name termodom--td-brain -d limitlesssoft/termodom--td-brain:%build.counter%
