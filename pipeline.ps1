c020520962afadd0621deab3744c28c544c5bb2b
Aleksa Ristic <32262570+DoLoop216@users.noreply.github.com>
2023-06-18 02:16:39 +0200

    Stage

--- pipeline.ps1
+++ pipeline.ps1
@@ -2,5 +2,5 @@ $MainDir = Get-location
 cd $MainDir/src/Termodom-TDBrain-v3
 docker stop termodom--td-brain
 docker rm termodom--td-brain
-docker build . -t limitlesssoft/termodom--td-brain:{build.number}
-docker run -p 32775:80 --name termodom--td-brain -d limitlesssoft/termodom--td-brain:{build.number}
+docker build . -t limitlesssoft/termodom--td-brain:$env:BUILD_NUMBER
+docker run -p 32775:80 --name termodom--td-brain -d limitlesssoft/termodom--td-brain:$env:BUILD_NUMBER
