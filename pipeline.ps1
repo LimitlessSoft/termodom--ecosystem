4cfae389de476eaf20b7c6ef5a064df7ce8c58e9
Aleksa Ristic <32262570+DoLoop216@users.noreply.github.com>
2023-06-18 02:46:32 +0200

    Stage

--- pipeline.ps1
+++ pipeline.ps1
@@ -9,5 +9,7 @@ docker run -p 32775:80 --name termodom--td-brain -m 1G --restart=always -d limit
 cd $MainDir/src/TD.Komercijalno/TD.Komercijalno.Api
 dotnet build
 dotnet publish -o obj/Docker/publish -c Release --runtime linux-x64 --self-contained False
+docker stop termodom--komercijalno-api
+docker rm termodom--komercijalno-api
 docker build -f ./Dockerfile -t limitlesssoft/termodom--komercijalno-api:$env:BUILD_NUMBER ./obj/Docker/publish
 docker run -p 32776:80 --name termodom--komercijalno-api -m 1G --restart=always -d limitlesssoft/termodom--komercijalno-api:$env:BUILD_NUMBER
