59349c50de5fd5d252fa3229fee7a83d298e255d
Aleksa Ristic <32262570+DoLoop216@users.noreply.github.com>
2023-06-19 15:28:17 +0200

    Stage

--- pipeline.ps1
+++ pipeline.ps1
@@ -1,16 +1,9 @@
 $MainDir = Get-location
-$asd = Get-Variable 'env.ConnectionStrings.KomercijalnoFransiza2023TCMD'
 
 echo test
-echo $env.ConnectionStrings.KomercijalnoFransiza2023TCMD
+echo %SomeParameter%
 echo test
-echo $env:env.ConnectionStrings.KomercijalnoFransiza2023TCMD
-echo test
-echo $env:env:ConnectionStrings:KomercijalnoFransiza2023TCMD
-echo test
-echo %env.ConnectionStrings.KomercijalnoFransiza2023TCMD%
-echo test
-echo %ConnectionStrings.KomercijalnoFransiza2023TCMD%
+echo %env.SomeEnvPar%
 echo test
 
 # cd $MainDir/src/Termodom-TDBrain-v3
