ae48f409243f5607ae897531b47391ff0a419d0e
Aleksa Ristic <32262570+DoLoop216@users.noreply.github.com>
2023-06-19 15:20:16 +0200

    Stage

--- pipeline.ps1
+++ pipeline.ps1
@@ -1,4 +1,9 @@
 $MainDir = Get-location
+$asd = Get-Variable 'env.ConnectionStrings.KomercijalnoFransiza2023TCMD'
+
+echo test
+echo $asd
+echo test
 
 # cd $MainDir/src/Termodom-TDBrain-v3
 # docker stop termodom--td-brain
@@ -12,7 +17,7 @@ dotnet publish -o obj/Docker/publish -c Release --runtime linux-x64 --self-conta
 docker stop termodom--komercijalno-api
 docker rm termodom--komercijalno-api
 docker build -f ./Dockerfile -t limitlesssoft/termodom--komercijalno-api:$env:BUILD_NUMBER ./obj/Docker/publish
-docker run -p 32776:80 -e ConnectionString_Komercijalno=%env.ConnectionStrings.KomercijalnoFransiza2023TCMD% --name termodom--komercijalno-api -m 1G --restart=always -d limitlesssoft/termodom--komercijalno-api:$env:BUILD_NUMBER
+docker run -p 32776:80 -e ConnectionString_Komercijalno=$asd --name termodom--komercijalno-api -m 1G --restart=always -d limitlesssoft/termodom--komercijalno-api:$env:BUILD_NUMBER
 
 cd $MainDir/src/TD.WebshopListener/TD.WebshopListener.App
 dotnet build
