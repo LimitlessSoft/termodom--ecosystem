07263955440aaebce4440cfca91e2f4c90e7f4b8
Aleksa Ristic <32262570+DoLoop216@users.noreply.github.com>
2023-07-15 15:16:01 +0200

    Stage

--- pipeline.ps1
+++ pipeline.ps1
@@ -34,7 +34,7 @@ cd $MainDir/src/TD.TDOffice/TD.TDOffice.Api
 dotnet build
 dotnet publish -o obj/Docker/publish -c Release --runtime linux-x64 --self-contained False
 docker build -f ./Dockerfile -t limitlesssoft/termodom--tdoffice-api:$env:BUILD_NUMBER ./obj/Docker/publish
-docker run -p 32778:80 --name termodom--tdoffice-api -m 1G --restart=always -d limitlesssoft/termodom--tdoffice-api:$env:BUILD_NUMBER
+docker run -p 32778:80 -e ConnectionString_TDOffice=$env:ConnectionStrings_TDOffice --name termodom--tdoffice-api -m 1G --restart=always -d limitlesssoft/termodom--tdoffice-api:$env:BUILD_NUMBER
 
 cd $MainDir/src/TD.FE/TD.FE.TDOffice/TD.FE.TDOffice.Api
 dotnet build
