ab217904f1367737621cf1b7dfdf6088aa06da1e
Aleksa Ristic <32262570+DoLoop216@users.noreply.github.com>
2023-07-27 03:22:10 +0200

    Stage

--- veleprodaja-pipeline.ps1
+++ veleprodaja-pipeline.ps1
@@ -7,4 +7,4 @@ cd $MainDir/src/WebApplications/TD.Web.Veleprodaja/TD.Web.Veleprodaja.Api
 dotnet build
 dotnet publish -o obj/Docker/publish -c Release --runtime linux-x64 --self-contained False
 docker build -f ./Dockerfile -t limitlesssoft/termodom--web-veleprodaja-api:$env:BUILD_NUMBER ./obj/Docker/publish
-docker run -p 33770:80 -e API_HOST=$env:API_HOST --name termodom--tdoffice-fe-api -m 1G --restart=always -d limitlesssoft/termodom--web-veleprodaja-api:$env:BUILD_NUMBER
+docker run -p 33770:80 -e API_HOST=$env:API_HOST --name termodom--tdoffice-web-veleprodaja-api -m 1G --restart=always -d limitlesssoft/termodom--web-veleprodaja-api:$env:BUILD_NUMBER
