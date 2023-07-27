8508887c6d9f2e5f1a7181eca0c1367ce844fb60
Aleksa Ristic <32262570+DoLoop216@users.noreply.github.com>
2023-07-27 03:15:19 +0200

    Stage

---
+++ src/TD.FE/TD.FE.TDOffice/TD.FE.TDOffice.Domain/Managers/TDWebVeleprodajaApiManager.cs
@@ -0,0 +1,24 @@
+﻿using TD.Core.Domain.Managers;
+using TD.FE.TDOffice.Contracts.IManagers;
+
+namespace TD.FE.TDOffice.Domain.Managers
+{
+    public class TDWebVeleprodajaApiManager : BaseApiManager, ITDWebVeleprodajaApiManager
+    {
+        public TDWebVeleprodajaApiManager()
+        {
+#if DEBUG
+            HttpClient.BaseAddress = new Uri("http://localhost:33770");
+#else
+            string? serverHost = Environment.GetEnvironmentVariable("API_HOST");
+
+            if (string.IsNullOrWhiteSpace(serverHost))
+            {
+                logger.LogCritical("Environment variable 'API_HOST' is not assigned!");
+                throw new ArgumentNullException(nameof(serverHost));
+            }
+            HttpClient.BaseAddress = new Uri($"http://{serverHost}:33770");
+#endif
+        }
+    }
+}
---
+++ veleprodaja-pipeline.ps1
@@ -0,0 +1,10 @@
+$MainDir = Get-location
+
+docker stop termodom--web-veleprodaja-api
+docker rm termodom--web-veleprodaja-api
+
+cd $MainDir/src/WebApplications/TD.Web.Veleprodaja/TD.Web.Veleprodaja.Api
+dotnet build
+dotnet publish -o obj/Docker/publish -c Release --runtime linux-x64 --self-contained False
+docker build -f ./Dockerfile -t limitlesssoft/termodom--web-veleprodaja-api:$env:BUILD_NUMBER ./obj/Docker/publish
+docker run -p 33770:80 -e API_HOST=$env:API_HOST --name termodom--tdoffice-fe-api -m 1G --restart=always -d limitlesssoft/termodom--web-veleprodaja-api:$env:BUILD_NUMBER
