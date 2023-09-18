83e64c2de549fcf0dc9c34c09373204db0cd5c3d
Aleksa Ristic <aristiccitsira@gmail.com>
2023-09-18 21:30:23 +0200

    Stage

--- src/TD.Core/TD.Core.Repository/Extensions.cs
+++ src/TD.Core/TD.Core.Repository/Extensions.cs
@@ -4,7 +4,8 @@ namespace TD.Core.Repository
 {
     public static class Extensions
     {
-        public static EntityTypeBuilder<TEntity> AddMap<TEntity>(this EntityTypeBuilder<TEntity> entityTypeBuilder, IEntityMap<TEntity> map) where TEntity : class
+        public static EntityTypeBuilder<TEntity> AddMap<TEntity>(this EntityTypeBuilder<TEntity> entityTypeBuilder, IEntityMap<TEntity> map)
+            where TEntity : class
         {
             return map.Map(entityTypeBuilder);
         }
