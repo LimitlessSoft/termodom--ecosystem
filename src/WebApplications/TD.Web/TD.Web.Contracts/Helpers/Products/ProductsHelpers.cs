13087bfb9c238edb20c28b5e298adbf9dac36b24
Aleksa <78086079+Milicke@users.noreply.github.com>
2023-09-18 20:11:02 +0200

    Fixed #192

--- src/WebApplications/TD.Web/TD.Web.Contracts/Helpers/Products/ProductsHelpers.cs
+++ src/WebApplications/TD.Web/TD.Web.Contracts/Helpers/Products/ProductsHelpers.cs
@@ -7,12 +7,12 @@ namespace TD.Web.Contracts.Helpers.Products
         public static string GenerateSrc(this string name)
         {
             var src = "";
-            src = Regex.Replace(name, Constants.RegexProductSrcPattern, Constants.RegexProductSrcReplacement)
-                       .Replace(Constants.RegexReplaceMultipleSpacesExpression, Constants.RegexReplaceMultipleSpacesReplacement)
-                       .Replace(Constants.RegexReplaceMultipleDashesPattern, Constants.RegexReplaceMultipleDashesReplacement);
-            src = src.StartsWith(Constants.RegexReplaceMultipleSpacesReplacement) ? src.Substring(1) : src;
-            src = src.EndsWith(Constants.RegexReplaceMultipleSpacesReplacement) ? src.Substring(0, src.Length - 1) : src;
-            return src;
+            src = Regex.Replace(name, Constants.RegexProductSrcPattern, Constants.RegexProductSrcReplacement);
+            src = Regex.Replace(src, Constants.RegexReplaceMultipleSpacesExpression, Constants.RegexReplaceMultipleSpacesReplacement);
+
+            return Regex.Replace(src, Constants.RegexReplaceMultipleDashesPattern, Constants.RegexReplaceMultipleDashesReplacement)
+                   .TrimStart(Constants.RegexReplaceMultipleSpacesReplacement.ToCharArray())
+                   .TrimEnd(Constants.RegexReplaceMultipleSpacesReplacement.ToCharArray());
         }
 
         public static bool isSrcValid(this string src)
