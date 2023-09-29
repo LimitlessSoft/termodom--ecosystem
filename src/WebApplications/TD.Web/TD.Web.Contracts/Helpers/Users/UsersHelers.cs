7d362c9ccd84199e31dd027a3834a2433aeeb413
Aleksa <78086079+Milicke@users.noreply.github.com>
2023-09-29 21:00:57 +0200

    UserEntity cleanup

---
+++ src/WebApplications/TD.Web/TD.Web.Contracts/Helpers/Users/UsersHelers.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TD.Web.Contracts.Helpers.Users
+{
+    public static class UsersHelers
+    {
+        public static bool IsUsernameNotValid(this string username)
+        {
+            return !Regex.IsMatch(username, Constants.RegexValidateUsernamePattern);
+        }
+        public static bool IsPasswordNotStrong(this string password)
+        {
+            return !Regex.IsMatch(password, Constants.RegexValidatePasswordPattern);
+        }
+    }
+}
