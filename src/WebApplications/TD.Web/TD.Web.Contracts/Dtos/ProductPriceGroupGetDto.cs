0d3a3218f93b21153e31e62b4871cabc1024e4b8
Aleksa <78086079+Milicke@users.noreply.github.com>
2023-10-05 19:39:26 +0200

    Route Setup

---
+++ src/WebApplications/TD.Web/TD.Web.Contracts/Dtos/ProductPriceGroupGetDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TD.Web.Contracts.Dtos
+{
+    public class ProductPriceGroupGetDto
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/WebApplications/TD.Web/TD.Web.Contracts/Requests/ProductPriceGroup/ProductPriceGroupSaveRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TD.Web.Contracts.Requests.ProductPriceGroup
+{
+    public class ProductPriceGroupSaveRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
