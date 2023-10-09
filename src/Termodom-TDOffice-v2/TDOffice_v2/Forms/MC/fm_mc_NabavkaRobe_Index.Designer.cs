17d9f94f437ea413dbbb7140bb57d12c63cf4106
Aleksa Ristic <aristiccitsira@gmail.com>
2023-10-09 14:23:56 +0200

    Stage

--- src/Termodom-TDOffice-v2/TDOffice_v2/Forms/MC/fm_mc_NabavkaRobe_Index.Designer.cs
+++ src/Termodom-TDOffice-v2/TDOffice_v2/Forms/MC/fm_mc_NabavkaRobe_Index.Designer.cs
@@ -30,6 +30,8 @@
         {
             components = new System.ComponentModel.Container();
             dataGridView1 = new System.Windows.Forms.DataGridView();
+            contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(components);
+            poveziSaRobomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             uvuciCenovnik_btn = new System.Windows.Forms.Button();
             comboBox1 = new System.Windows.Forms.ComboBox();
             label1 = new System.Windows.Forms.Label();
@@ -39,13 +41,12 @@
             label3 = new System.Windows.Forms.Label();
             numericUpDown3 = new System.Windows.Forms.NumericUpDown();
             label4 = new System.Windows.Forms.Label();
-            contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(components);
-            poveziSaRobomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            contextMenuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
-            contextMenuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // dataGridView1
@@ -53,91 +54,11 @@
             dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
             dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.ContextMenuStrip = contextMenuStrip1;
-            dataGridView1.Location = new System.Drawing.Point(12, 128);
+            dataGridView1.Location = new System.Drawing.Point(12, 153);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new System.Drawing.Size(948, 399);
+            dataGridView1.Size = new System.Drawing.Size(948, 374);
             dataGridView1.TabIndex = 0;
-            // 
-            // uvuciCenovnik_btn
-            // 
-            uvuciCenovnik_btn.Location = new System.Drawing.Point(280, 57);
-            uvuciCenovnik_btn.Name = "uvuciCenovnik_btn";
-            uvuciCenovnik_btn.Size = new System.Drawing.Size(106, 65);
-            uvuciCenovnik_btn.TabIndex = 1;
-            uvuciCenovnik_btn.Text = "Izaberi Fajl";
-            uvuciCenovnik_btn.UseVisualStyleBackColor = true;
-            uvuciCenovnik_btn.Click += uvuciCenovnik_btn_Click;
-            // 
-            // comboBox1
-            // 
-            comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "PROIZVODJAC1" });
-            comboBox1.Location = new System.Drawing.Point(86, 12);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new System.Drawing.Size(208, 23);
-            comboBox1.TabIndex = 2;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(12, 15);
-            label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(68, 15);
-            label1.TabIndex = 3;
-            label1.Text = "Proizvodjac";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new System.Drawing.Point(12, 44);
-            label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(171, 15);
-            label2.TabIndex = 4;
-            label2.Text = "Kolona Kat. Br. (A = 1, B = 2 ... )";
-            // 
-            // numericUpDown1
-            // 
-            numericUpDown1.Location = new System.Drawing.Point(189, 41);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new System.Drawing.Size(85, 23);
-            numericUpDown1.TabIndex = 5;
-            numericUpDown1.Value = new decimal(new int[] { 3, 0, 0, 0 });
-            // 
-            // numericUpDown2
-            // 
-            numericUpDown2.Location = new System.Drawing.Point(181, 70);
-            numericUpDown2.Name = "numericUpDown2";
-            numericUpDown2.Size = new System.Drawing.Size(85, 23);
-            numericUpDown2.TabIndex = 7;
-            numericUpDown2.Value = new decimal(new int[] { 4, 0, 0, 0 });
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new System.Drawing.Point(12, 73);
-            label3.Name = "label3";
-            label3.Size = new System.Drawing.Size(163, 15);
-            label3.TabIndex = 6;
-            label3.Text = "Kolona Naziv (A = 1, B = 2 ... )";
-            // 
-            // numericUpDown3
-            // 
-            numericUpDown3.Location = new System.Drawing.Point(167, 99);
-            numericUpDown3.Name = "numericUpDown3";
-            numericUpDown3.Size = new System.Drawing.Size(85, 23);
-            numericUpDown3.TabIndex = 9;
-            numericUpDown3.Value = new decimal(new int[] { 5, 0, 0, 0 });
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new System.Drawing.Point(12, 102);
-            label4.Name = "label4";
-            label4.Size = new System.Drawing.Size(149, 15);
-            label4.TabIndex = 8;
-            label4.Text = "Kolona JM (A = 1, B = 2 ... )";
             // 
             // contextMenuStrip1
             // 
@@ -152,11 +73,102 @@
             poveziSaRobomToolStripMenuItem.Text = "Povezi sa robom";
             poveziSaRobomToolStripMenuItem.Click += poveziSaRobomToolStripMenuItem_Click;
             // 
+            // uvuciCenovnik_btn
+            // 
+            uvuciCenovnik_btn.Location = new System.Drawing.Point(211, 66);
+            uvuciCenovnik_btn.Name = "uvuciCenovnik_btn";
+            uvuciCenovnik_btn.Size = new System.Drawing.Size(160, 78);
+            uvuciCenovnik_btn.TabIndex = 1;
+            uvuciCenovnik_btn.Text = "Izaberi Fajl";
+            uvuciCenovnik_btn.UseVisualStyleBackColor = true;
+            uvuciCenovnik_btn.Click += uvuciCenovnik_btn_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "RU Metal Trade d.o.o", "Exim Commerce doo" });
+            comboBox1.Location = new System.Drawing.Point(77, 12);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new System.Drawing.Size(208, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(12, 15);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(59, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Dobavljac";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(16, 66);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(84, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Kolona Kat. Br.";
+            label2.Click += label2_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new System.Drawing.Point(106, 64);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new System.Drawing.Size(85, 23);
+            numericUpDown1.TabIndex = 5;
+            numericUpDown1.Value = new decimal(new int[] { 2, 0, 0, 0 });
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new System.Drawing.Point(98, 93);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new System.Drawing.Size(85, 23);
+            numericUpDown2.TabIndex = 7;
+            numericUpDown2.Value = new decimal(new int[] { 3, 0, 0, 0 });
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(16, 95);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(76, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Kolona Naziv";
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Location = new System.Drawing.Point(84, 122);
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new System.Drawing.Size(85, 23);
+            numericUpDown3.TabIndex = 9;
+            numericUpDown3.Value = new decimal(new int[] { 5, 0, 0, 0 });
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(16, 124);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(62, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Kolona JM";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(16, 40);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(214, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Vrednosti kolona - A = 1, B = 2, C = 3 ...";
+            // 
             // fm_mc_NabavkaRobe_Index
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             ClientSize = new System.Drawing.Size(972, 539);
+            Controls.Add(label5);
             Controls.Add(numericUpDown3);
             Controls.Add(label4);
             Controls.Add(numericUpDown2);
@@ -171,10 +183,10 @@
             Text = "fm_mc_NabavkaRobe_Index";
             Load += fm_mc_NabavkaRobe_Index_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            contextMenuStrip1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
-            contextMenuStrip1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -193,5 +205,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem poveziSaRobomToolStripMenuItem;
+        private System.Windows.Forms.Label label5;
     }
 }
