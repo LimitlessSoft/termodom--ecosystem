f0542fa79faf7c883f68e09ba0977d86ac9e64e8
PC <aleksamilickv@gmail.com>
2023-10-19 10:21:33 +0200

    Roles

--- src/WebApplications/TD.Web/TD.Web.Api/Controllers/OrdersController.cs
+++ src/WebApplications/TD.Web/TD.Web.Api/Controllers/OrdersController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using TD.Core.Contracts.Http;
 using TD.Web.Contracts.Dtos.Orders;
 using TD.Web.Contracts.Enums;
+using TD.Web.Contracts.Helpers.Roles;
 using TD.Web.Contracts.Interfaces.IManagers;
 
 namespace TD.Web.Api.Controllers
@@ -12,6 +13,7 @@ namespace TD.Web.Api.Controllers
     public class OrdersController : ControllerBase
     {
         private readonly IOrderManager _orderManager;
+        public const string admin = "admin";
         
         public OrdersController(IOrderManager orderManager, IHttpContextAccessor httpContextAccessor)
         {
---
+++ src/WebApplications/TD.Web/TD.Web.Contracts/Helpers/Roles/RolesHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace TD.Web.Contracts.Helpers.Roles
+{
+    public static class RolesHelper
+    {
+        public static string CombineRoles(params string[] roles)
+        {
+            return string.Join(",", roles);
+        }
+    }
+}
