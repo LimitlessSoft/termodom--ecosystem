e710d54a9ce4a7bdbd9ff5a3b23228803b94d607
Aleksa <78086079+Milicke@users.noreply.github.com>
2023-10-19 22:29:31 +0200

    Roles cleanup

--- src/WebApplications/TD.Web/TD.Web.Contracts/Enums/UserRoles.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TD.Web.Contracts.Enums
-{
-    public static class UserRoles
-    {
-        public const string User = "User";
-        public const string Admin = "Admin";
-        public const string SuperAdmin = "SuperAdmin";
-    }
-}
