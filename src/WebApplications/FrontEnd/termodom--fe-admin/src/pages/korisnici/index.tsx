f83fae1ebf37f20b7b9f0e5f76535df89e6c71a2
Aleksa Ristic <aristiccitsira@gmail.com>
2023-10-22 20:16:01 +0200

    Stage

---
+++ src/WebApplications/FrontEnd/termodom--fe-admin/src/pages/korisnici/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material"
+
+const Korisnici = (): JSX.Element => {
+    return (
+        <Box>
+            Korisnici
+        </Box>
+    )
+}
+
+
+export default Korisnici
