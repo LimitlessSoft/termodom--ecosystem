caa06e2289136e53c30bd93d5f8550e16734cb95
Aleksa Ristic <aristiccitsira@gmail.com>
2023-10-22 02:49:10 +0200

    Hotfix

--- src/WebApplications/FrontEnd/termodom--fe-admin/src/pages/proizvodi/novi/index.tsx
+++ src/WebApplications/FrontEnd/termodom--fe-admin/src/pages/proizvodi/novi/index.tsx
@@ -17,68 +17,70 @@ const ProizvodiNovi = (): JSX.Element => {
 
 
     return (
-        <Box
-        sx={{ m: 2, '& .MuiTextField-root': { m: 1, width: '25ch' }, }}>
-            <TextField
-            required
-            id='name'
-            label='Ime proizvoda'
-            variant={textFieldVariant} />
+        <div>
+            <Box
+            sx={{ m: 2, '& .MuiTextField-root': { m: 1, width: '25ch' }, }}>
+                <TextField
+                required
+                id='name'
+                label='Ime proizvoda'
+                variant={textFieldVariant} />
 
-            <TextField
-            required
-            id='src'
-            label='link (src)'
-            variant={textFieldVariant} />
+                <TextField
+                required
+                id='src'
+                label='link (src)'
+                variant={textFieldVariant} />
 
-            <TextField
-            required
-            id='catalogId'
-            label='Kataloški broj'
-            variant={textFieldVariant} />
+                <TextField
+                required
+                id='catalogId'
+                label='Kataloški broj'
+                variant={textFieldVariant} />
 
-            <TextField
-            id='unit'
-            select
-            required
-            label='Jedinica mere'
-            helperText='Izaberite jedinicu mere proizvoda'>
-                {units?.map((unit:any, index:any) => {
-                    return (
-                        <MenuItem key={`f123sfa${index}`} value={unit.id}>
-                            {unit.name}
-                        </MenuItem>
-                    )
-                })}
-            </TextField>
+                <TextField
+                id='unit'
+                select
+                required
+                label='Jedinica mere'
+                helperText='Izaberite jedinicu mere proizvoda'>
+                    {units?.map((unit:any, index:any) => {
+                        return (
+                            <MenuItem key={`f123sfa${index}`} value={unit.id}>
+                                {unit.name}
+                            </MenuItem>
+                        )
+                    })}
+                </TextField>
 
-            <TextField
-            id='classification'
-            select
-            required
-            label='Klasifikacija'
-            helperText='Izaberite klasu proizvoda'>
-                <MenuItem value={0}>
-                    Hobi
-                </MenuItem>
-                <MenuItem value={1}>
-                    Standard
-                </MenuItem>
-                <MenuItem value={2}>
-                    Profi
-                </MenuItem>
-            </TextField>
+                <TextField
+                id='classification'
+                select
+                required
+                label='Klasifikacija'
+                helperText='Izaberite klasu proizvoda'>
+                    <MenuItem value={0}>
+                        Hobi
+                    </MenuItem>
+                    <MenuItem value={1}>
+                        Standard
+                    </MenuItem>
+                    <MenuItem value={2}>
+                        Profi
+                    </MenuItem>
+                </TextField>
 
-            <TextField
-            required
-            id='vat'
-            label='PDV'
-            variant={textFieldVariant} />
+                <TextField
+                required
+                id='vat'
+                label='PDV'
+                variant={textFieldVariant} />
 
-            grupe
+                grupe
 
-            productpricegrupe
-        </Box>
+                productpricegrupe
+            </Box>
+        </div>
     )
 }
 
