4d696dcc8bda02c291b9d0ed7679af4e9101c0a4
Aleksa Ristic <aristiccitsira@gmail.com>
2023-10-28 03:03:34 +0200

    TD.Web.Public projects initial setup

---
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Api/Controllers/PingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TD.Core.Contracts.Http;
+
+namespace TD.Web.Public.Api.Controllers
+{
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        public PingController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("/ping")]
+        public Response Ping()
+        {
+            return new Response();
+        }
+    }
+}
