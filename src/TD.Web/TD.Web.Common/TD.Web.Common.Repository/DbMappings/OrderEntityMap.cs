cf184e98530307b62ad4f4e14d805b8849c55962
Aleksa Ristic <aristiccitsira@gmail.com>
2023-10-28 03:09:27 +0200

    Stage

---
+++ src/TD.Web/TD.Web.Common/TD.Web.Common.Repository/DbMappings/OrderEntityMap.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TD.Core.Repository;
+using TD.Web.Admin.Contracts.Entities;
+
+namespace TD.Web.Admin.Repository.DbMappings
+{
+    public class OrderEntityMap : EntityMap<OrderEntity>
+    {
+        public override EntityTypeBuilder<OrderEntity> Map(EntityTypeBuilder<OrderEntity> entityTypeBuilder)
+        {
+            base.Map(entityTypeBuilder);
+
+            entityTypeBuilder
+                .HasOne(x => x.UserEntity)
+                .WithMany(x => x.Orders) 
+                .HasForeignKey(x => x.UserId);
+
+            entityTypeBuilder
+                .Property(x => x.UserId)
+                .IsRequired();
+
+            entityTypeBuilder
+                .Property(x => x.Status)
+                .IsRequired();
+
+            entityTypeBuilder
+                .Property(x => x.Date)
+                .IsRequired();
+
+            return entityTypeBuilder;
+        }
+    }
+}
