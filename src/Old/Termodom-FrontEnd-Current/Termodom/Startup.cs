c3e7e8904b23020d7eb04db7b0ba76ddf4b5ec0d
Aleksa <78086079+Milicke@users.noreply.github.com>
2023-11-16 20:41:59 +0100

    Middleware

--- src/Old/Termodom-FrontEnd-Current/Termodom/Startup.cs
+++ src/Old/Termodom-FrontEnd-Current/Termodom/Startup.cs
@@ -67,14 +67,14 @@ namespace Termodom
             if (env.IsDevelopment())
             {
                 Program.APIUsername = "sasar";
-                Program.APIPassword = "12321";
+                //Program.APIPassword = "12321";
                 Program.BaseAPIUrl = "https://api.termodom.rs";
                 //Program.BaseAPIUrl = "https://localhost:44311";
 
-                //Program.APIUsername = "termodom_webshop_dev";
-                //Program.APIPassword = "j7U4LBMqEf6X";
+                Program.APIUsername = "termodom_webshop_dev";
+                Program.APIPassword = "j7U4LBMqEf6X";
 
-                //Program.BaseAPIUrl = "https://api.termodom.rs";
+                Program.BaseAPIUrl = "https://api.termodom.rs";
                 app.UseDeveloperExceptionPage();
             }
             else
