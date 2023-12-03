b1623f22372d42d14bf976a88f0b80f0908d7022
Aleksa Ristic <aristiccitsira@gmail.com>
2023-12-03 04:24:25 +0100

    Cities entity

---
+++ src/TD.Web/TD.Web.Common/TD.Web.Common.Contracts/Entities/CityEntity.cs
@@ -0,0 +1,9 @@
+﻿using LSCore.Contracts.Entities;
+
+namespace TD.Web.Common.Contracts.Entities
+{
+    public class CityEntity : LSCoreEntity
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/TD.Web/TD.Web.Common/TD.Web.Common.DbMigrations/Migrations/20231203030104_CreateCityEntity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TD.Web.Common.DbMigrations.Migrations
+{
+    public partial class CreateCityEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatedBy = table.Column<int>(type: "integer", nullable: false),
+                    UpdatedBy = table.Column<int>(type: "integer", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
