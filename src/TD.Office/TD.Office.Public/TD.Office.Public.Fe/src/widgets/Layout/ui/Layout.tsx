9db2fe19764c8f44bbcbf1577fda3da0edec029d
Aleksa Ristic <aristiccitsira@gmail.com>
2023-12-24 13:51:44 +0100

    Stage

---
+++ src/TD.Office/TD.Office.Public/TD.Office.Public.Fe/src/widgets/Layout/ui/Layout.tsx
@@ -0,0 +1,23 @@
+import { Header } from "@/widgets/Header"
+import Head from "next/head"
+import { ReactNode } from "react"
+
+interface ILayoutProps {
+    children: ReactNode
+}
+
+export const Layout = (props: ILayoutProps): JSX.Element => {
+
+    const { children } = props
+
+    return (
+        <div className={`mainWrapper`}>
+            <Head>
+                <title>TDOffice</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+            </Head>
+            <Header />
+            <main>{children}</main>
+        </div>
+    )
+}
