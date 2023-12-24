b79350b02c921ef8d0e007f1d85a5499e3823891
Aleksa Ristic <aristiccitsira@gmail.com>
2023-12-24 14:02:52 +0100

    Stage

--- src/TD.Office/TD.Office.Public/TD.Office.Public.Fe/next.config.js
+++ src/TD.Office/TD.Office.Public/TD.Office.Public.Fe/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     publicRuntimeConfig: {
-        // API_BASE_URL_MAIN: "localhost:59011",
-        API_BASE_URL_MAIN: "http://localhost:5195"
+        API_BASE_URL_MAIN: "http://localhost:59011",
+        // API_BASE_URL_MAIN: "http://localhost:5195"
     }}
 
 module.exports = nextConfig
