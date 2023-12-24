264b8462591d049458b4ee0b082bc9ba2a38af94
Aleksa Ristic <aristiccitsira@gmail.com>
2023-12-24 04:18:45 +0100

    Added env parameters to office-pipeline

--- office-pipeline.ps1
+++ office-pipeline.ps1
@@ -30,7 +30,7 @@ cd $MainDir/src/TD.Office/TD.Office.Public/TD.Office.Public.Api
 dotnet build
 dotnet publish -o obj/Docker/publish -c Release --runtime linux-x64 --self-contained False
 docker build -f ./Dockerfile -t $ImageName_OfficeApiDotnet ./obj/Docker/publish
-docker run -p $ContainerHostPort_OfficeApiDotnet -e JWT_ISSUER=$env:JWT_ISSUER -e POSTGRES_HOST=$env:POSTGRES_HOST -e POSTGRES_PORT=$env:POSTGRES_PORT -e POSTGRES_PASSWORD=$env:POSTGRES_PASSWORD -e JWT_AUDIENCE=$env:JWT_AUDIENCE -e JWT_KEY=$env:JWT_KEY --name $ContainerName_OfficeApiDotnet -m 1G --restart=unless-stopped -d $ImageName_OfficeApiDotnet
+docker run -p $ContainerHostPort_OfficeApiDotnet -e JWT_ISSUER=$env:JWT_ISSUER -e POSTGRES_HOST=$env:POSTGRES_HOST -e POSTGRES_PORT=$env:POSTGRES_PORT -e POSTGRES_USER=$env:POSTGRES_USER -e POSTGRES_DATABASE_NAME=$env:POSTGRES_DATABASE_NAME -e POSTGRES_PASSWORD=$env:POSTGRES_PASSWORD -e JWT_AUDIENCE=$env:JWT_AUDIENCE -e JWT_KEY=$env:JWT_KEY --name $ContainerName_OfficeApiDotnet -m 1G --restart=unless-stopped -d $ImageName_OfficeApiDotnet
 
 # cd $MainDir/src/TD.Web/TD.Web.Admin/TD.Web.Admin.Fe
 # docker build -t $ImageName_WebFrontEndAdmin .
