c04b00f4355be01de510d756b12e7024a7843996
Aleksa Milic <aleksamilickv@gmail.com>
2023-12-26 19:25:24 +0100

    Price on Single Product EP

--- src/TD.Web/TD.Web.Public/TD.Web.Public.Contrats/Dtos/Products/ProductsGetSingleDto.cs
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Contrats/Dtos/Products/ProductsGetSingleDto.cs
@@ -1,4 +1,5 @@
 ﻿using LSCore.Contracts.Dtos;
+using TD.Web.Common.Contracts.Dtos;
 using TD.Web.Common.Contracts.Enums;
 
 namespace TD.Web.Public.Contracts.Dtos.Products
@@ -13,6 +14,8 @@ namespace TD.Web.Public.Contracts.Dtos.Products
         public string Unit { get; set; }
         public string? AlternateUnit { get; set; }
         public decimal? OneAlternatePackageEquals { get; set; }
+        public OneTimePricesDto OneTimePrice { get; set; }
+        public UserPricesDto Price { get; set; }
         public LSCoreFileDto? ImageData { get; set; }
         public ProductClassification Classification { get; set; }
     }
