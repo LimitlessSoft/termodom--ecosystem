0afd2a5287f2f7e76df2a3196b9beb2e9fdbd561
Aleksa Ristic <aristiccitsira@gmail.com>
2024-01-09 16:31:24 +0100

    Stage

---
+++ src/TD.Komercijalno/TD.Komercijalno.Api/Controllers/RobaUMagacinuController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TD.Komercijalno.Api.Controllers
+{
+    [ApiController]
+    public class RobaUMagacinuController : Controller
+    {
+        public RobaUMagacinuController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/TD.Komercijalno/TD.Komercijalno.Domain/Managers/RobaUMagacinuManager.cs
+++ src/TD.Komercijalno/TD.Komercijalno.Domain/Managers/RobaUMagacinuManager.cs
@@ -1,7 +1,12 @@
-﻿using LSCore.Domain.Managers;
-using Microsoft.Extensions.Logging;
-using TD.Komercijalno.Contracts.IManagers;
+﻿using LSCore.Contracts.Http;
+using LSCore.Domain.Managers;
+using LSCore.Domain.Extensions;
 using TD.Komercijalno.Repository;
+using LSCore.Contracts.Extensions;
+using Microsoft.Extensions.Logging;
+using TD.Komercijalno.Contracts.Entities;
+using TD.Komercijalno.Contracts.IManagers;
+using TD.Komercijalno.Contracts.Dtos.RobaUMagacinu;
 
 namespace TD.Komercijalno.Domain.Managers
 {
@@ -11,6 +16,21 @@ namespace TD.Komercijalno.Domain.Managers
             : base(logger, dbContext)
         {
 
+        }
+
+        public LSCoreListResponse<RobaUMagacinuGetDto> GetMultiple()
+        {
+            var response = new LSCoreListResponse<RobaUMagacinuGetDto>();
+
+            var qResponse = Queryable<RobaUMagacinu>();
+            response.Merge(qResponse);
+            if(response.NotOk)
+                return response;
+
+            response.Payload = qResponse.Payload!
+                .ToDtoList<RobaUMagacinuGetDto, RobaUMagacinu>();
+
+            return response;
         }
     }
 }
