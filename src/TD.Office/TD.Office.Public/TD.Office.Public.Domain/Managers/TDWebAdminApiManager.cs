af473aaf55a1a9773b44c509f75f3543079806d6
Aleksa Ristic <aristiccitsira@gmail.com>
2024-01-13 17:34:01 +0100

    Stage

--- src/TD.Office/TD.Office.Public/TD.Office.Public.Domain/Managers/TDWebAdminApiManager.cs
+++ src/TD.Office/TD.Office.Public/TD.Office.Public.Domain/Managers/TDWebAdminApiManager.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using TD.Office.Public.Contracts.Interfaces.IManagers;
 using TD.Web.Admin.Contracts.Dtos.KomercijalnoWebProductLinks;
 using TD.Web.Admin.Contracts.Dtos.Products;
+using TD.Web.Admin.Contracts.Requests.KomercijalnoWebProductLinks;
 
 namespace TD.Office.Public.Domain.Managers
 {
@@ -16,6 +17,9 @@ namespace TD.Office.Public.Domain.Managers
 
         public Task<LSCoreResponse<List<KomercijalnoWebProductLinksGetDto>>> KomercijalnoKomercijalnoWebProductsLinksGetMultipleAsync() =>
             base.GetAsync<List<KomercijalnoWebProductLinksGetDto>>("/komercijalno-web-product-links");
+
+        public Task<LSCoreResponse<KomercijalnoWebProductLinksGetDto>> KomercijalnoWebProductLinksControllerPutAsync(KomercijalnoWebProductLinksSaveRequest request) =>
+            base.PutAsync<KomercijalnoWebProductLinksSaveRequest, KomercijalnoWebProductLinksGetDto>("/komercijalno-web-product-links", request);
 
         public Task<LSCoreResponse<List<Web.Admin.Contracts.Dtos.Products.ProductsGetDto>>> ProductsGetMultipleAsync() =>
             base.GetAsync<List<ProductsGetDto>>("/products");
