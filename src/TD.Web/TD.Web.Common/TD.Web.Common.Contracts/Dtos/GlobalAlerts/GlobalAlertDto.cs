44aec481783fa77d40bf1b403e45d6d2896969fe
Aleksa Ristic <aristiccitsira@gmail.com>
2024-01-29 04:15:53 +0100

    Stage

---
+++ src/TD.Web/TD.Web.Common/TD.Web.Common.Contracts/Dtos/GlobalAlerts/GlobalAlertDto.cs
@@ -0,0 +1,7 @@
+﻿namespace TD.Web.Common.Contracts.Dtos.GlobalAlerts
+{
+    public class GlobalAlertDto
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Api/Controllers/GlobalAlertsController.cs
@@ -0,0 +1,24 @@
+﻿using LSCore.Contracts.Http;
+using Microsoft.AspNetCore.Mvc;
+using TD.Web.Common.Contracts.Dtos.GlobalAlerts;
+using TD.Web.Common.Contracts.Interfaces.IManagers;
+using TD.Web.Common.Contracts.Requests.GlobalAlerts;
+
+namespace TD.Web.Public.Api.Controllers
+{
+    [ApiController]
+    public class GlobalAlertsController : ControllerBase
+    {
+        private readonly IGlobalAlertManager _globalAlertManager;
+
+        public GlobalAlertsController(IGlobalAlertManager globalAlertManager)
+        {
+            _globalAlertManager = globalAlertManager;
+        }
+
+        [HttpGet]
+        [Route("/global-alerts")]
+        public LSCoreListResponse<GlobalAlertDto> GetMultiple([FromQuery] GlobalAlertsGetMultipleRequest request) =>
+            _globalAlertManager.GetMultiple(request);
+    }
+}
