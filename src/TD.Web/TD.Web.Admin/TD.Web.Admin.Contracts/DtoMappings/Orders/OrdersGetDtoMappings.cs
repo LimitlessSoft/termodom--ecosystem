497db539a4f68c58ff12d5361c0891d7e66048e2
Aleksa Milic <aleksamilickv@gmail.com>
2024-02-10 14:47:38 +0100

    Translate Status on /orders

--- src/TD.Web/TD.Web.Admin/TD.Web.Admin.Contracts/DtoMappings/Orders/OrdersGetDtoMappings.cs
+++ src/TD.Web/TD.Web.Admin/TD.Web.Admin.Contracts/DtoMappings/Orders/OrdersGetDtoMappings.cs
@@ -1,4 +1,5 @@
-﻿using LSCore.Contracts.Interfaces;
+﻿using LSCore.Contracts.Extensions;
+using LSCore.Contracts.Interfaces;
 using TD.Web.Admin.Contracts.Dtos.Orders;
 using TD.Web.Common.Contracts.Entities;
 
@@ -11,7 +12,7 @@ namespace TD.Web.Admin.Contracts.DtoMappings.Orders
             {
                 OneTimeHash = sender.OneTimeHash,
                 CreatedAt = sender.CreatedAt,
-                Status = sender.Status.ToString(),
+                Status = sender.Status.GetDescription()!,
                 User = (sender.OrderOneTimeInformation != null) ? sender.OrderOneTimeInformation.Name : sender.User.Nickname,
                 ValueWithVAT = sender.Items.Sum(x => (x.Price * x.Quantity * ((x.Product.VAT + 100) / 100))),
                 DiscountValue = sender.Items.Sum(x => ((x.PriceWithoutDiscount - x.Price) * x.Quantity))
--- src/TD.Web/TD.Web.Common/TD.Web.Common.Contracts/Enums/OrderStatus.cs
+++ src/TD.Web/TD.Web.Common/TD.Web.Common.Contracts/Enums/OrderStatus.cs
@@ -1,11 +1,20 @@
-﻿namespace TD.Web.Common.Contracts.Enums
+﻿using System.ComponentModel;
+
+namespace TD.Web.Common.Contracts.Enums
 {
     public enum OrderStatus
     {
+        [Description("Otvorena")]
         Open = 0,
+        [Description("Čeka obradu")]
         PendingReview = 1,
-        InProcess = 2,
-        Completed = 3,
-        Canceled = 4
+        [Description("Na obradi")]
+        InReview = 2,
+        [Description("Spremna za preuzimanje")]
+        WaitingCollection = 3,
+        [Description("Preuzeta")]
+        Collected = 4,
+        [Description("Otkazan")]
+        Canceled = 5
     }
 }
