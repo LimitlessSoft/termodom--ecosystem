1e8fe01ab7bf7901e4f6bea0515b5a3768dabbde
Aleksa Milic <aleksamilickv@gmail.com>
2024-02-14 19:05:04 +0100

    EnumController

---
+++ src/TD.Web/TD.Web.Admin/TD.Web.Admin.Api/Controllers/EnumController.cs
@@ -0,0 +1,22 @@
+﻿using LSCore.Contracts.Dtos;
+using LSCore.Contracts.Http;
+using Microsoft.AspNetCore.Mvc;
+using TD.Web.Admin.Contracts.Interfaces.IManagers;
+
+namespace TD.Web.Admin.Api.Controllers
+{
+    [ApiController]
+    public class EnumController : ControllerBase
+    {
+        private readonly IEnumManager _enumManager;
+        public EnumController(IEnumManager enumManager)
+        {
+            _enumManager = enumManager;
+        }
+
+        [HttpGet]
+        [Route("/order-statuses")]
+        public LSCoreListResponse<LSCoreIdNamePairDto> GetOrderStatuses() =>
+            _enumManager.GetOrderStatuses();
+    }
+}
