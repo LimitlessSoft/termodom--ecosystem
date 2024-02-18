e004baee6b7f6e47b6d545b7c947f9f2901746a0
Aleksa Ristic <aristiccitsira@gmail.com>
2024-02-18 21:55:02 +0100

    Stage

--- src/TD.Web/TD.Web.Admin/TD.Web.Admin.Fe/next.config.js
+++ src/TD.Web/TD.Web.Admin/TD.Web.Admin.Fe/next.config.js
@@ -5,8 +5,7 @@ const getApiBaseUrlMain = () => {
         case 'stage':
             return 'https://api-admin-stage.termodom.rs'
         case 'develop':
-            // return 'https://api-admin-develop.termodom.rs'
-            return 'http://localhost:5219'
+            return 'https://api-admin-develop.termodom.rs'
         default:
             return 'error-loading-deployment-environment'
     }
