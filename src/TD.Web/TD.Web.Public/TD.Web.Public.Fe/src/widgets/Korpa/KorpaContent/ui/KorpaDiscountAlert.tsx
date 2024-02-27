31c34c9297894896942cf3af350af657b124149a
Aleksa Ristic <aristiccitsira@gmail.com>
2024-02-27 19:25:15 +0100

    Stage

--- src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/widgets/Korpa/KorpaContent/ui/KorpaDiscountAlert.tsx
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/widgets/Korpa/KorpaContent/ui/KorpaDiscountAlert.tsx
@@ -1,6 +1,7 @@
 import { ApiBase, fetchApi } from "@/app/api"
 import { CookieNames } from "@/app/constants"
 import { formatNumber } from "@/app/helpers/numberHelpers"
+import { ResponsiveTypography } from "@/widgets/Responsive"
 import { Grid, LinearProgress, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import useCookie from 'react-use-cookie'
@@ -21,12 +22,19 @@ export const KorpaDiscountAlert = (props: any): JSX.Element => {
         props.cart == null || currentCartLevel == null ?
         <LinearProgress /> :
         <Grid m={5}>
-            <Typography
-                align={`justify`}>
-                Trenutna ukupna vrednost vašeg računa bez PDV-a iznosi {formatNumber(props.cart.summary.valueWithoutVAT)} i dodeljeni su Vam rabati stepena {currentCartLevel.currentLevel}.
-                Ukoliko ukupna vrednost računa pređe {formatNumber(currentCartLevel.nextLevelValue)} RSD stepen rabata će biti ažuriran!
-                *Rabat se obracunava na ukupnu vrednost korpe bez pdv-a
-            </Typography>
+            {
+                currentCartLevel.nextLevelValue == null ?
+                    <ResponsiveTypography
+                        align={`center`}>
+                        Vaša korpa je trenutno na najvišem nivou rabata!
+                    </ResponsiveTypography> :
+                    <ResponsiveTypography
+                        align={`justify`}>
+                        Trenutna ukupna vrednost vašeg računa bez PDV-a iznosi {formatNumber(props.cart.summary.valueWithoutVAT)} i dodeljeni su Vam rabati stepena {currentCartLevel.currentLevel}.
+                        Ukoliko ukupna vrednost računa pređe {formatNumber(currentCartLevel.nextLevelValue)} RSD stepen rabata će biti ažuriran!
+                        *Rabat se obracunava na ukupnu vrednost korpe bez pdv-a
+                    </ResponsiveTypography>
+            }
         </Grid>
     )
 }
