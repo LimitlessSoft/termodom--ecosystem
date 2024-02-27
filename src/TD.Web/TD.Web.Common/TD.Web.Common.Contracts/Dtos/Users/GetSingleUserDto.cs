d146302b0992f25ee751c9ebfef649602c0e7cd4
Aleksa Milic <aleksamilickv@gmail.com>
2024-02-27 19:43:18 +0100

    Single User

---
+++ src/TD.Web/TD.Web.Common/TD.Web.Common.Contracts/Dtos/Users/GetSingleUserDto.cs
@@ -0,0 +1,21 @@
+﻿using LSCore.Contracts.Dtos;
+
+namespace TD.Web.Common.Contracts.Dtos.Users
+{
+    public class GetSingleUserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Nickname { get; set; }
+        public LSCoreIdNamePairDto? Profession { get; set; }
+        public string? PIB { get; set; }
+        public int? PPID { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public LSCoreIdNamePairDto City { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+        public string? Mail { get; set; }
+        public LSCoreIdNamePairDto FavoriteStore { get; set; }
+        public string? Comment { get; set; }
+    }
+}
