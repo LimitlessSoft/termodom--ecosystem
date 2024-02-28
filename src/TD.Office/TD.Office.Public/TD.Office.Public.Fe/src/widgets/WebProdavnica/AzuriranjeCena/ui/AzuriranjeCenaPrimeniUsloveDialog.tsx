825d97a7d6c09185a0221391faed66ef502663b7
Aleksa Ristic <aristiccitsira@gmail.com>
2024-02-28 17:55:32 +0100

    Hotfix

--- src/TD.Office/TD.Office.Public/TD.Office.Public.Fe/src/widgets/WebProdavnica/AzuriranjeCena/ui/AzuriranjeCenaPrimeniUsloveDialog.tsx
+++ src/TD.Office/TD.Office.Public/TD.Office.Public.Fe/src/widgets/WebProdavnica/AzuriranjeCena/ui/AzuriranjeCenaPrimeniUsloveDialog.tsx
@@ -1,3 +1,4 @@
+import { mainTheme } from "@/app/themes"
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from "@mui/material"
 
 interface IAzuriranjeCenaPrimeniUsloveDialogProps {
@@ -16,6 +17,10 @@ export const AzuriranjeCenaPrimeniUsloveDialog = (props: IAzuriranjeCenaPrimeniU
             <DialogContent>
                 <Typography>
                     Da li ste sigurni da želite da primenite uslove formiranja Min Web Osnove?
+                </Typography>
+                <Typography py={5} color={mainTheme.palette.warning.main} textAlign={`justify`}>
+                    Pre ove akcije obavezno pokrenite 'AŽURIRAJ MAX WEB OSNOVE' jer ukoliko min osnova bude veća od max osnove,
+                    min osnova će biti postavljena na vrednost max osnove (tako da ako je max osnova 0, i min osnova ce biti 0 uvek)
                 </Typography>
             </DialogContent>
             <DialogActions>
