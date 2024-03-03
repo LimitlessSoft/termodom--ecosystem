11b5d1c8e1b9f54a72ff89bf173aa3147ff864b0
Aleksa Ristic <aristiccitsira@gmail.com>
2024-03-03 10:47:13 +0100

    Stage

---
+++ src/TD.Web/TD.Web.Admin/TD.Web.Admin.Api/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using TD.Web.Common.Contracts.Interfaces.IManagers;
+using TD.Web.Common.Contracts.Requests.Cities;
+using TD.Web.Common.Contracts.Dtos.Cities;
+using Microsoft.AspNetCore.Mvc;
+using LSCore.Contracts.Http;
+
+namespace TD.Web.Admin.Api.Controllers
+{
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly ICityManager _cityManager;
+        
+        public CitiesController(ICityManager cityManager)
+        {
+            _cityManager = cityManager;
+        }
+
+        [HttpGet]
+        [Route("/cities")]
+        public LSCoreListResponse<CityDto> GetMultiple([FromQuery] GetMultipleCitiesRequest request) =>
+            _cityManager.GetMultiple(request);
+    }
+}
--- src/TD.Web/TD.Web.Admin/TD.Web.Admin.Fe/src/widgets/Korisnici/Korisnik/ui/KorisnikBody.tsx
+++ src/TD.Web/TD.Web.Admin/TD.Web.Admin.Fe/src/widgets/Korisnici/Korisnik/ui/KorisnikBody.tsx
@@ -3,8 +3,51 @@ import moment from "moment"
 import { KorisnikBodyInfoDataWrapperStyled } from "./KorisnikBodyInfoDataWrapperStyled"
 import { DatePicker } from "@mui/x-date-pickers"
 import { mainTheme } from "@/app/theme"
+import { useEffect, useRef, useState } from "react"
+import { ApiBase, fetchApi } from "@/app/api"
+import dayjs from "dayjs"
 
 export const KorisnikBody = (props: any): JSX.Element => {
+
+    const putUserRequest = useRef<any>({})
+
+    const [professions, setProfessions] = useState<any | undefined>(undefined)
+    const [stores, setStores] = useState<any | undefined>(undefined)
+
+    useEffect(() => {
+        fetchApi(ApiBase.Main, `/professions`)
+        .then((r) => {
+            setProfessions(r)
+        })
+
+        fetchApi(ApiBase.Main, `/stores`)
+        .then((r) => {
+            setStores(r)
+        })
+    }, [])
+
+    useEffect(() => {
+        props.user === undefined ? 
+            putUserRequest.current = undefined :
+            putUserRequest.current = {
+                id: props.user.id,
+                username: props.user.username,
+                nickname: props.user.nickname,
+                professionId: props.user.profession.id,
+                ppid: props.user.ppid,
+                dateOfBirth: props.user.dateOfBirth,
+                cityId: props.user.cityId,
+                address: props.user.address,
+                mobile: props.user.mobile,
+                mail: props.user.mail,
+                favoriteStoreId: props.user.favoriteStoreId,
+                comment: props.user.comment,
+                type: props.user.type,
+                isActive: props.user.isActive,
+                referentId: props.user.referentId,
+            }
+    }, [props.user])
+
     return (
         props.user === undefined ?
             <CircularProgress /> :
@@ -22,13 +65,13 @@ export const KorisnikBody = (props: any): JSX.Element => {
                         }}
                         direction={`column`}>
                         <Typography>
-                            Datum kreiranja naloga: {moment(props.user.createdAt).format("DD.MM.yyyy")}
+                            Datum kreiranja naloga: {moment(props.user.createdAt).format("DD.MM.yyyy (HH:mm)")}
                         </Typography>
                         <Typography>
-                            Datum obrade: { props.user.processingDate !== null ? moment(props.user.processingDate).format("DD.MM.yyyy") : "Još uvek nije obrađen"}
+                            Datum obrade: { props.user.processingDate !== null ? moment(props.user.processingDate).format("DD.MM.yyyy (HH:mm)") : "Još uvek nije obrađen"}
                         </Typography>
                         <Typography>
-                            Poslednji put viđen: { props.user.lastTimeSeen !== null ? moment(props.user.lastTimeSeen).format("DD.MM.yyyy") : "Nikada"}
+                            Poslednji put viđen: { props.user.lastTimeSeen !== null ? moment(props.user.lastTimeSeen).format("DD.MM.yyyy (HH:mm)") : "Nikada"}
                         </Typography>
                     </Grid>
                 </Grid>
@@ -40,22 +83,39 @@ export const KorisnikBody = (props: any): JSX.Element => {
                             <TextField
                                 variant={`filled`}
                                 defaultValue={props.user.nickname}
-                                label={`Nadimak`} />
+                                label={`Nadimak`}
+                                onChange={(e) => {
+                                    putUserRequest.current.nickname = e.target.value
+                                }} />
                         </KorisnikBodyInfoDataWrapperStyled>
                         <KorisnikBodyInfoDataWrapperStyled>
-                            <TextField
-                                select
-                                variant={`filled`}
-                                defaultValue={props.user.profession ?? 1}
-                                label={`Zanimanje`}>
-                                    <MenuItem value={1}>Programer</MenuItem>
-                            </TextField>
+                            {
+                                professions === undefined ?
+                                    <CircularProgress /> :
+                                    <TextField
+                                        select
+                                        variant={`filled`}
+                                        defaultValue={props.user.profession.id}
+                                        label={`Zanimanje`}
+                                        onChange={(e) => {
+                                            putUserRequest.current.professionId = e.target.value
+                                        }}>
+                                        {
+                                            professions.map((p: any, index: number) => (
+                                                <MenuItem key={index} value={p.id}>{p.name}</MenuItem>
+                                            ))
+                                        }
+                                    </TextField>
+                            }
                         </KorisnikBodyInfoDataWrapperStyled>
                         <KorisnikBodyInfoDataWrapperStyled>
                             <TextField
                                 variant={`filled`}
                                 defaultValue={props.user.ppid}
-                                label={`PPID`} />
+                                label={`PPID`}
+                                onChange={(e) => {
+                                    putUserRequest.current.ppid = e.target.value
+                                }} />
                         </KorisnikBodyInfoDataWrapperStyled>
                         <Stack p={`5px`}>
                             <Typography>
@@ -66,7 +126,9 @@ export const KorisnikBody = (props: any): JSX.Element => {
                                     maxWidth: 200,
                                     width: `100%`
                                 }}
+                                defaultValue={dayjs(props.user.dateOfBirth)}
                                 onChange={(e: any) => {
+                                    putUserRequest.current.dateOfBirth = e
                                 }}/>
                         </Stack>
                         <KorisnikBodyInfoDataWrapperStyled>
@@ -74,7 +136,10 @@ export const KorisnikBody = (props: any): JSX.Element => {
                                 select
                                 variant={`filled`}
                                 defaultValue={props.user.city.id}
-                                label={`Mesto`}>
+                                label={`Mesto`}
+                                onChange={(e) => {
+                                    putUserRequest.current.cityId = e.target.value
+                                }}>
                                     <MenuItem value={53}>Beograd</MenuItem>
                             </TextField>
                         </KorisnikBodyInfoDataWrapperStyled>
@@ -82,28 +147,48 @@ export const KorisnikBody = (props: any): JSX.Element => {
                             <TextField
                                 variant={`filled`}
                                 defaultValue={props.user.address}
-                                label={`Adresa`} />
+                                label={`Adresa`}
+                                onChange={(e) => {
+                                    putUserRequest.current.address = e.target.value
+                                }}/>
                         </KorisnikBodyInfoDataWrapperStyled>
                         <KorisnikBodyInfoDataWrapperStyled>
                             <TextField
                                 variant={`filled`}
                                 defaultValue={props.user.mobile}
-                                label={`Mobilni`} />
+                                label={`Mobilni`}
+                                onChange={(e) => {
+                                    putUserRequest.current.mobile = e.target.value
+                                }} />
                         </KorisnikBodyInfoDataWrapperStyled>
                         <KorisnikBodyInfoDataWrapperStyled>
                             <TextField
                                 variant={`filled`}
                                 defaultValue={props.user.mail}
-                                label={`Mail`} />
+                                label={`Mail`}
+                                onChange={(e) => {
+                                    putUserRequest.current.mail = e.target.value
+                                }} />
                         </KorisnikBodyInfoDataWrapperStyled>
                         <KorisnikBodyInfoDataWrapperStyled>
-                            <TextField
-                                select
-                                variant={`filled`}
-                                defaultValue={props.user.favoriteStore.id}
-                                label={`Omiljena radnja`}>
-                                    <MenuItem value={117}>Beograd</MenuItem>
-                            </TextField>
+                            {
+                                stores === undefined ?
+                                    <CircularProgress /> :
+                                    <TextField
+                                        select
+                                        variant={`filled`}
+                                        defaultValue={props.user.favoriteStore.id}
+                                        label={`Omiljena radnja`}
+                                        onChange={(e) => {
+                                            putUserRequest.current.favoriteStoreId = e.target.value
+                                        }}>
+                                        {
+                                            stores.map((s: any, index: number) => (
+                                                <MenuItem key={index} value={s.id}>{s.name}</MenuItem>
+                                            ))
+                                        }
+                                    </TextField>
+                            }
                         </KorisnikBodyInfoDataWrapperStyled>
                         <KorisnikBodyInfoDataWrapperStyled>
                             <TextField
@@ -111,10 +196,16 @@ export const KorisnikBody = (props: any): JSX.Element => {
                                 fullWidth={true}
                                 minRows={4}
                                 defaultValue={props.user.favoriteStore.id}
-                                label={`Komentar`} />
+                                label={`Komentar`}
+                                onChange={(e) => {
+                                    putUserRequest.current.comment = e.target.value
+                                }} />
                         </KorisnikBodyInfoDataWrapperStyled>
                         <Button
-                            variant={`contained`}>
+                            variant={`contained`}
+                            onClick={() => {
+                                console.log(putUserRequest.current)
+                            }}>
                             Sačuvaj
                         </Button>
                 </Grid>
