04da043a0faf2e2488d8e1e8e6c5ef611ef18a6e
Aleksa Ristic <aristiccitsira@gmail.com>
2024-03-03 01:42:22 +0100

    fix

--- src/TD.Web/TD.Web.Common/TD.Web.Common.Contracts/DtoMappings/Users/UserProductPriceLevelsDtoMappings.cs
+++ src/TD.Web/TD.Web.Common/TD.Web.Common.Contracts/DtoMappings/Users/UserProductPriceLevelsDtoMappings.cs
@@ -1,28 +1,16 @@
-﻿using LSCore.Contracts.Http;
-using LSCore.Contracts.Interfaces;
-using System.Runtime.CompilerServices;
-using TD.Web.Common.Contracts.Dtos.Users;
+﻿using TD.Web.Common.Contracts.Dtos.Users;
 using TD.Web.Common.Contracts.Entities;
 
 namespace TD.Web.Common.Contracts.DtoMappings.Users
 {
     public static class UserProductPriceLevelsDtoMappings
     {
-        public static List<UserProductPriceLevelsDto> ToUserPriceLevelsDto(this List<ProductPriceGroupLevelEntity> sender, List<ProductPriceGroupEntity> groups)
-        {
-            var response = new List<UserProductPriceLevelsDto>();
-
-            foreach (var group in groups)
-                response.Add(new UserProductPriceLevelsDto()
+        public static List<UserProductPriceLevelsDto> ToUserPriceLevelsDto(this List<ProductPriceGroupLevelEntity> sender, List<ProductPriceGroupEntity> groups) =>
+            groups.Select(group => new UserProductPriceLevelsDto()
                 {
                     GroupId = group.Id,
-                    Level = 0
-                });
-
-            foreach (var item in sender)
-                response.Where(x => x.GroupId == item.ProductPriceGroupId).FirstOrDefault()!.Level = item.Level;    
-
-            return response;
-        }
+                    Level = sender.First(x => x.ProductPriceGroupId == group.Id).Level
+                })
+                .ToList();
     }
 }
