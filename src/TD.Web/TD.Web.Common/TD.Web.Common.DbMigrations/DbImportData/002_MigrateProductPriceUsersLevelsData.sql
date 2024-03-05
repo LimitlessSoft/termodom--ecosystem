b3a8a0c1c5ded5074fe6cdd58ab184a32028ebf5
Aleksa Ristic <aristiccitsira@gmail.com>
2024-03-05 23:05:16 +0100

    Migrating user prices

---
+++ src/TD.Web/TD.Web.Common/TD.Web.Common.DbMigrations/DbImportData/002_MigrateProductPriceUsersLevelsData.sql
@@ -0,0 +1,12 @@
+﻿INSERT INTO "ProductPriceGroupLevel" ("UserId", "Level", "ProductPriceGroupId", "IsActive", "CreatedAt", "CreatedBy")
+   select
+   u."Id",
+   ouc.nivo,
+   case when (ouc.cenovnik_grupaid + 1) > 8 THEN 7 ELSE (ouc.cenovnik_grupaid + 1) end as "GrupaId",
+   true,
+   current_timestamp,
+   0
+       from "old_user_cenovnik" ouc
+       left join "old_users" ou on ouc.userid = ou.id
+       left join "Users" u on ou.ime = u."Username"
+       where u."Id" is not null
