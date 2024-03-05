9e3b7f8a86088c40e3c292ee912355c6fc454f86
Aleksa Ristic <aristiccitsira@gmail.com>
2024-03-05 22:32:57 +0100

    Legacy password verifyer allowed

--- src/TD.Web/TD.Web.Common/TD.Web.Common.Domain/Validators/UserLoginRequestValidator.cs
+++ src/TD.Web/TD.Web.Common/TD.Web.Common.Domain/Validators/UserLoginRequestValidator.cs
@@ -1,4 +1,6 @@
-﻿using FluentValidation;
+﻿using System.Security.Cryptography;
+using System.Text;
+using FluentValidation;
 using LSCore.Contracts.Extensions;
 using LSCore.Domain.Validators;
 using TD.Web.Common.Contracts.Enums.ValidationCodes;
@@ -15,6 +17,30 @@ namespace TD.Web.Common.Domain.Validators.Users
                 .Custom((request, context) =>
                 {
                     var user = dbContext.Users.FirstOrDefault(x => x.Username.ToUpper() == request.Username.ToUpper());
+
+                    #region Checking legacy login. If legacy login, updating user password with new implementation so it can be validated
+
+                    if (user != null && LegacyHashPassword(request.Password) == user.Password)
+                    {
+                        user.Password = BCrypt.Net.BCrypt.EnhancedHashPassword(request.Password);
+                        dbContext.SaveChanges();
+                    }
+                    
+                    string LegacySimpleHash(string value)
+                    {
+                        HashAlgorithm algorithm = SHA256.Create();
+                        byte[] res = algorithm.ComputeHash(Encoding.UTF8.GetBytes(value));
+                        StringBuilder sb = new StringBuilder();
+                        foreach (byte b in res)
+                            sb.Append(b.ToString("X2"));
+
+                        return sb.ToString();
+                    }
+                    string LegacyHashPassword(string RawPassword)
+                    {
+                        return LegacySimpleHash(LegacySimpleHash(LegacySimpleHash(LegacySimpleHash(LegacySimpleHash(LegacySimpleHash(RawPassword))))));
+                    }
+                    #endregion
 
                     if (user == null || !BCrypt.Net.BCrypt.EnhancedVerify(request.Password, user.Password))
                     {
