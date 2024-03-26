237ff7a880d11a946fc98e0dd2c32432a35e87be
Aleksa Ristic <aristiccitsira@gmail.com>
2024-03-26 19:39:48 +0100

    Stage

--- src/TD.WebshopListener/TD.WebshopListener.Contracts/Constants/WebApiEndpoints.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TD.WebshopListener.Contracts.Constants
-{
-    public static class WebApiEndpoints
-    {
-        public static string GetToken(string username, string password) => $"/api/Korisnik/GetToken?username={username}&password={password}";
-        public static string GetAkcList() => $"/api/akc/list";
-    }
-}
--- src/TD.WebshopListener/TD.WebshopListener.Contracts/Messages/CommonMessages.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TD.WebshopListener.Contracts.Messages
-{
-    public static class CommonMessages
-    {
-        public static string ActionNotHandledMessage(string action) => $"'{action}' action is not handled!";
-    }
-}
