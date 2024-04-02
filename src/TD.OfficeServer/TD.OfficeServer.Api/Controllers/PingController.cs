3b196c22feeab44953dd83ed1ccd253bd6681a9e
Aleksa Ristic <aristiccitsira@gmail.com>
2024-04-02 02:52:21 +0200

    Stage

--- src/TD.OfficeServer/TD.OfficeServer.Api/Controllers/PingController.cs
+++ src/TD.OfficeServer/TD.OfficeServer.Api/Controllers/PingController.cs
@@ -1,22 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using LSCore.Contracts.Http;
-using TDBrain_v3;
+using TD.OfficeServer.Contracts.IManagers;
 
 namespace TD.OfficeServer.Api.Controllers
 {
     [ApiController]
     public class PingController : ControllerBase
     {
+        public PingController()
+        {
+        }
+
         [HttpGet]
         [Route("/ping")]
         public LSCoreResponse<string> Ping() => new LSCoreResponse<string>("Pong!");
-
-        [HttpGet]
-        [Route("/test-sms")]
-        public LSCoreResponse<string> TestSms()
-        {
-            GSMModem.QueueSMS(new List<string>() {"0693691472"}, "Hello test");
-            return new LSCoreResponse<string>();
-        }
     }
 }
