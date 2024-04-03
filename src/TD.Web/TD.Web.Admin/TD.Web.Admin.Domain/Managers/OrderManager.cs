f6203ce944c3ddf30238b85f6e373d89a54a1df3
Aleksa Ristic <aristiccitsira@gmail.com>
2024-04-03 23:08:51 +0200

    Stage

--- src/TD.Web/TD.Web.Admin/TD.Web.Admin.Domain/Managers/OrderManager.cs
+++ src/TD.Web/TD.Web.Admin/TD.Web.Admin.Domain/Managers/OrderManager.cs
@@ -222,7 +222,7 @@ namespace TD.Web.Admin.Domain.Managers
             _officeServerApiManager.SMSQueueAsync(new SMSQueueRequest()
             {
                 Numbers = new List<string>() { order.OrderOneTimeInformation == null ? order.User.Mobile : order.OrderOneTimeInformation!.Mobile }, 
-                Text = $"Vaša porudžbina {order.OneTimeHash[..5]} je obrađena. TD Broj: " + dokument.BrDok,
+                Text = $"Vasa porudzbina {order.OneTimeHash[..5]} je obradjena. TD Broj: " + dokument.BrDok,
             });
             
             return new LSCoreResponse();
