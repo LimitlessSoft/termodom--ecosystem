6fc05ee8b7c33b2c1c4a572cbf9970a565a97203
Aleksa Ristic <aristiccitsira@gmail.com>
2024-04-13 08:24:13 +0200

    Stage

--- src/TD.SMSGateway/Readme.md
+++ src/TD.SMSGateway/Readme.md
@@ -1,6 +1,6 @@
 Install node
 Install pm2
 Start application.js with:
-	pm2 start -l /path-to-file-you-want-to-log --cron-restart="* * * * *"
+	pm2 start application.js --cron-restart="* * * * *" -l /path-to-file-you-want-to-log 
 
 which will start app each minute
--- src/TD.SMSGateway/application.js
+++ src/TD.SMSGateway/application.js
@@ -2,19 +2,10 @@
 const BULK_SMS_COUNT = 3
 // ======
 
-// function
-const getSecondsDifference = (startDate, endDate) => {
-	const startMS = startDate.getTime()
-	const endMS = endDate.getTime()
-	return (endMS - startMS) / 1000
-}
-// =====
 const process = require('process')
-setTimeout(() => {
-	process.exit(0)
-}, 58 * 1000)
-
 const serialPortGSM = require('serialport-gsm')
+const Firebird = require('node-firebird')
+
 const modem = serialPortGSM.Modem()
 const modemOptions = {
 	baudRate: 115200,
@@ -37,7 +28,6 @@ const modemOptions = {
 modem.setModemMode(null, 'SMS')
 modem.open('COM3', modemOptions)
 
-const Firebird = require('node-firebird')
 const options = {
 	host: '4monitor',
 	port: 3050,
@@ -48,9 +38,10 @@ const options = {
 	encoding: "UTF-8"
 }
 
-const queueSms = (remainingSms, db) => {
-	const sms = remainingSms.pop()
+const queueSms = (remainingSms, db, onAllSmsSent) => {
+	let sms = remainingSms.pop()
 	let firstCallback = true
+
 	modem.sendSMS(sms.MOBILE, sms.TEXT, false, (e) => {
 
 		if(firstCallback)
@@ -59,41 +50,59 @@ const queueSms = (remainingSms, db) => {
 			return
 		}
 
-		db.query("UPDATE SMS SET STATUS = " + (e.status == 'success' ? 1 : 7) + " WHERE ID = " + sms.ID, (err, result) => {
+		db.query("UPDATE SMS SET STATUS = " + (e.status == 'success' ? 1 : 7)
+			+ " WHERE ID = " + sms.ID, (err, result) => {
 			if(err)
 				throw err
-		})
 
-		if(remainingSms.length == 0) {
-			modem.close()
-			db.detach()
-			process.exit(0)
+			if(remainingSms.length == 0 && onAllSmsSent != null)
+			{
+				onAllSmsSent()
+				return
+			}
+			queueSms(remainingSms, db)
+		})
+	})
+}
+
+const prepareSmsToSend = (db) => {
+	// Select first last 10 SMSs pending to be sent
+	db.query("SELECT FIRST " + BULK_SMS_COUNT
+		+ " * FROM SMS WHERE STATUS = 2 ORDER BY ID DESC", function(err, result) {
+
+		if(err)
+			throw err
+
+		if(result.length == 0)
+		{
+			setTimeout(() => {
+				prepareSmsToSend(db)
+			}, 3000)
 			return
 		}
 
-		queueSms(remainingSms, db)
+		queueSms(result, db, () => { prepareSmsToSend(db) })
+
 	})
 }
 
 modem.on('open', data => {
 	modem.initializeModem()
 
-	Firebird.attach(options, function(err, db) {
-		if(err)
-			throw err
+	try
+	{
+		Firebird.attach(options, function(err, db) {
+			if(err)
+				throw err
 
-		// Select first last 10 SMSs pending to be sent
-		db.query("SELECT FIRST " + BULK_SMS_COUNT + " * FROM SMS WHERE STATUS = 2 ORDER BY ID DESC", function(err, result) {
+			prepareSmsToSend(db)
 
-			if(result.length == 0) {
-				modem.close()
-				db.detach()
-				process.exit(0)
-			}
-
-			queueSms(result, db)
-
-			setTimeout(() => { process.exit(0) }, 60 * 1000)
 		})
-	})
+	}
+	catch(ex)
+	{
+		console.error(ex)
+		modem.close()
+		db.detach()
+	}
 })
