27930e1017442206c82d09016b80856a2e5f8bb4
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-07-01 14:46:12 +0200

    Created a back button for single order

---
+++ src/TD.Web/TD.Web.Admin/TD.Web.Admin.Fe/src/widgets/BackButton/ui/BackButton.tsx
@@ -0,0 +1,15 @@
+import { Box, Button, Link, Typography } from "@mui/material"
+import NextLink from 'next/link';
+
+
+export const BackButton = (): JSX.Element => {
+    return (
+      <Box sx={{m: 2}}>
+        {
+            <Button variant="contained" LinkComponent={NextLink} href=".">
+                <Typography>Nazad</Typography>
+            </Button> 
+        }
+      </Box>
+    )
+}
