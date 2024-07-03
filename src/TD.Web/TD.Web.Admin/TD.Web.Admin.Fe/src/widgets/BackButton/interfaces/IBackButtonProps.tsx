eb8e946ab50a316d2216890e4c42e2fca648e325
Aleksa Ristic <aristiccitsira@gmail.com>
2024-07-03 22:29:13 +0200

    Stage

--- src/TD.Web/TD.Web.Admin/TD.Web.Admin.Fe/src/widgets/BackButton/interfaces/IBackButtonProps.tsx
+++ src/TD.Web/TD.Web.Admin/TD.Web.Admin.Fe/src/widgets/BackButton/interfaces/IBackButtonProps.tsx
@@ -1,3 +1,12 @@
+import {OverridableStringUnion} from "@mui/types";
+import {ButtonPropsColorOverrides} from "@mui/material/Button/Button";
+
 export interface IBackButtonProps {
-    href:string
+    href: string,
+    disableStartIcon?: boolean,
+    color?: OverridableStringUnion<
+        'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning',
+        ButtonPropsColorOverrides
+    >,
+    text?: string
 }
