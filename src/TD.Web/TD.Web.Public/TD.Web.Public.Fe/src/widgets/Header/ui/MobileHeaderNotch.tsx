6222591aa844e208c3d73a8538ea2ed74d08e15c
Aleksa Ristic <aristiccitsira@gmail.com>
2024-07-03 22:54:52 +0200

    Stage

--- src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/widgets/Header/ui/MobileHeaderNotch.tsx
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/widgets/Header/ui/MobileHeaderNotch.tsx
@@ -4,7 +4,7 @@ import { Grid, Typography } from "@mui/material"
 
 export const MobileHeaderNotch = (props: IClickable): JSX.Element => {
     return (
-        <Grid flexDirection={`row`} container alignItems={`center`}>
+        <Grid container alignItems={`center`}>
             <MobileHeaderNotchStyled
                 onClick={() => {
                     props.onClick()
@@ -13,7 +13,9 @@ export const MobileHeaderNotch = (props: IClickable): JSX.Element => {
                 <span></span>
                 <span></span>
             </MobileHeaderNotchStyled>
-            <Typography component={`h2`} variant="subtitle1" color={`black`} fontSize={32}>Meni</Typography>
+            <Typography component={`span`} variant={`subtitle1`} color={`black`} fontSize={{
+                xs: 32
+            }}>Meni</Typography>
         </Grid>
     )
 }
