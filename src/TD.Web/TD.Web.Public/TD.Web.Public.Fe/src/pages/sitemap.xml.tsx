f33a16ea22062daf3a66c941ef639ce0c5c7b24b
Aleksa Ristic <aristiccitsira@gmail.com>
2024-07-18 13:00:37 +0200

    Fixed sitemap

--- src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/pages/sitemap.xml.tsx
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/pages/sitemap.xml.tsx
@@ -1,7 +1,7 @@
-import { ApiBase, fetchApi } from "@/app/api";
+import { ApiBase, fetchApi } from '@/app/api'
 
 function generateSiteMap(proizvodi: any[]) {
-  return `<?xml version="1.0" encoding="UTF-8"?>
+    return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     
     <url>
@@ -27,47 +27,48 @@ function generateSiteMap(proizvodi: any[]) {
         <lastmod>2024-05-13T18:20:59+01:00</lastmod>
         <priority>0.2</priority>
     </url>
-    ${
-        proizvodi.map((proizvod) => {
+    ${proizvodi
+        .map((proizvod) => {
             return `
                 <url>
                     <loc>https://termodom.rs/proizvodi/${proizvod.src}</loc>
                     <lastmod>2024-05-13T18:20:59+01:00</lastmod>
                     <priority>0.5</priority>
                 </url>
-            `;
-        }).join('')
-    }
+            `
+        })
+        .join('')}
 
    </urlset>
- `;
+ `
 }
 
 function SiteMap() {
-  // getServerSideProps will do the heavy lifting
+    // getServerSideProps will do the heavy lifting
 }
 
 export async function getServerSideProps({ res }: any) {
-  // We make an API call to gather the URLs for our site
-  let products: any[]
-  const productsResponse = await fetchApi(ApiBase.Main, '/products?pageSize=10000')
-    .then((response) =>
-    {
-        products = response
-    })
-    .catch((error) => {
-        console.error(error)
-        products = []
-    });
+    // We make an API call to gather the URLs for our site
+    let products: any[]
+    await fetchApi(ApiBase.Main, '/products?pageSize=10000')
+        .then(async (response) => {
+            await response.json().then((data: any) => {
+                products = data.payload
+            })
+        })
+        .catch((error) => {
+            console.error(error)
+            products = []
+        })
 
-  res.setHeader('Content-Type', 'text/xml');
-  // we send the XML to the browser
-  res.write(generateSiteMap(products!));
-  res.end();
+    res.setHeader('Content-Type', 'text/xml')
+    // we send the XML to the browser
+    res.write(generateSiteMap(products!))
+    res.end()
 
-  return {
-    props: {},
-  };
+    return {
+        props: {},
+    }
 }
 
-export default SiteMap;
+export default SiteMap
