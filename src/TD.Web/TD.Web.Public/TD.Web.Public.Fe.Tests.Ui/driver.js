299239055831c0c50b1d35c0af39103c52130584
Aleksa Ristic <aristiccitsira@gmail.com>
2024-08-13 01:28:08 +0200

    Stage

--- src/TD.Web/TD.Web.Public/TD.Web.Public.Fe.Tests.Ui/driver.js
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Fe.Tests.Ui/driver.js
@@ -1,4 +1,43 @@
-import webdriver, { Capabilities } from 'selenium-webdriver'
+import webdriver, { Builder, Capabilities } from 'selenium-webdriver'
+import Chrome from 'selenium-webdriver/chrome.js'
+import Firefox from 'selenium-webdriver/firefox.js'
 
-export const createDriver = async () => await new webdriver.Builder().withCapabilities(Capabilities.firefox()
-        .set("acceptInsecureCerts", true)).build()
+const createLocalDriver = () => {
+    if (process.env.BROWSER === 'firefox') {
+        let options = new Firefox.Options()
+        options.addArguments('-headless')
+        return new webdriver.Builder()
+            .withCapabilities(Capabilities.firefox().set("acceptInsecureCerts", true))
+            .setFirefoxOptions(options)
+            .build()
+    } else if (process.env.BROWSER === 'chrome') {
+        let options = new Chrome.Options()
+        options.addArguments('--headless')
+        return new webdriver.Builder()
+            .withCapabilities(Capabilities.chrome().set("acceptInsecureCerts", true))
+            .setChromeOptions(options)
+            .build()
+    } else {
+        throw new Error('Unsupported browser')
+    }
+}
+
+const createRemoteDriver = () => {
+    const seleniumServer = process.env.SELENIUM_SERVER || 'selenium'
+    return new Builder()
+        .usingServer(`http://${seleniumServer}:4444`)
+        .forBrowser('chrome')
+        .build();
+}
+
+export const createDriver = () => {
+    if (process.env.ENV === 'local') {
+        return createLocalDriver()
+    }
+    else if (process.env.ENV === 'github-action') {
+        return createRemoteDriver()
+    }
+    else {
+        throw new Error('Unsupported environment')
+    }
+}
