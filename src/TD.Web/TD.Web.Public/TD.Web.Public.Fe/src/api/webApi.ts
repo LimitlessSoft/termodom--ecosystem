913af617619c82d63a23b1bd3aa33a6e3f7d0fbb
Aleksa Ristic <aristiccitsira@gmail.com>
2024-08-12 22:44:40 +0200

    Stage

--- src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/api/webApi.ts
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/api/webApi.ts
@@ -2,7 +2,7 @@ import { getCookie } from 'react-use-cookie'
 import { COOKIES } from '@/constants'
 import axios from 'axios'
 
-const webApi = axios.create({
+export const webApi = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_MAIN_URL,
     headers: {
         Authorization: `Bearer ${getCookie(COOKIES.TOKEN.NAME)}`,
