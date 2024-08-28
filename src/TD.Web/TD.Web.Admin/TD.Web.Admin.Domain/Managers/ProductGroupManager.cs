0e81055b2a50a9a4f2bdb87e567196b2d55f4d92
Aleksa Ristic <aristiccitsira@gmail.com>
2024-08-28 08:14:27 +0200

    Stage

--- src/TD.Web/TD.Web.Admin/TD.Web.Admin.Domain/Managers/ProductGroupManager.cs
+++ src/TD.Web/TD.Web.Admin/TD.Web.Admin.Domain/Managers/ProductGroupManager.cs
@@ -1,33 +1,53 @@
 ﻿using LSCore.Contracts;
-using TD.Web.Admin.Contracts.Requests.ProductsGroups;
-using TD.Web.Admin.Contracts.Interfaces.IManagers;
-using TD.Web.Admin.Contracts.Dtos.ProductsGroups;
-using TD.Web.Common.Contracts.Entities;
-using Microsoft.Extensions.Logging;
 using LSCore.Contracts.Exceptions;
 using LSCore.Contracts.Requests;
 using LSCore.Domain.Extensions;
-using TD.Web.Common.Repository;
 using LSCore.Domain.Managers;
+using Microsoft.Extensions.Logging;
+using TD.Web.Admin.Contracts.Dtos.ProductsGroups;
+using TD.Web.Admin.Contracts.Helpers.ProductGroups;
+using TD.Web.Admin.Contracts.Interfaces.IManagers;
+using TD.Web.Admin.Contracts.Requests.ProductsGroups;
+using TD.Web.Common.Contracts.Entities;
+using TD.Web.Common.Repository;
 
 namespace TD.Web.Admin.Domain.Managers;
 
-public class ProductGroupManager (ILogger<ProductGroupManager> logger, WebDbContext dbContext, LSCoreContextUser contextUser)
-    : LSCoreManagerBase<ProductGroupManager, ProductGroupEntity>(logger, dbContext, contextUser), IProductGroupManager
+public class ProductGroupManager(
+    ILogger<ProductGroupManager> logger,
+    WebDbContext dbContext,
+    LSCoreContextUser contextUser
+)
+    : LSCoreManagerBase<ProductGroupManager, ProductGroupEntity>(logger, dbContext, contextUser),
+        IProductGroupManager
 {
     public ProductsGroupsGetDto Get(LSCoreIdRequest request) =>
         Queryable()
-            .FirstOrDefault(x => x.Id == request.Id && x.IsActive)?
-            .ToDto<ProductGroupEntity, ProductsGroupsGetDto>()
+            .FirstOrDefault(x => x.Id == request.Id && x.IsActive)
+            ?.ToDto<ProductGroupEntity, ProductsGroupsGetDto>()
         ?? throw new LSCoreNotFoundException();
 
     public List<ProductsGroupsGetDto> GetMultiple() =>
-        Queryable()
-            .Where(x => x.IsActive)
-            .ToDtoList<ProductGroupEntity, ProductsGroupsGetDto>();
+        Queryable().Where(x => x.IsActive).ToDtoList<ProductGroupEntity, ProductsGroupsGetDto>();
 
-    public long Save(ProductsGroupsSaveRequest request) => 
-        Save(request, (entity) => entity.Id);
+    public long Save(ProductsGroupsSaveRequest request)
+    {
+        var entity =
+            request.Id == 0
+                ? new ProductGroupEntity()
+                : Queryable().FirstOrDefault(x => x.Id == request.Id);
+
+        if (entity == null)
+            throw new LSCoreNotFoundException();
+
+        entity.Name = request.Name;
+        entity.ParentGroupId = request.ParentGroupId;
+        entity.WelcomeMessage = request.WelcomeMessage;
+        entity.Src = ProductGroupsHelpers.FormatName(request.Name);
+
+        Insert(entity);
+        return entity.Id;
+    }
 
     public void Delete(ProductsGroupsDeleteRequest request)
     {
@@ -35,6 +55,5 @@ public class ProductGroupManager (ILogger<ProductGroupManager> logger, WebDbCont
         HardDelete(request.Id);
     }
 
-    public void UpdateType(ProductsGroupUpdateTypeRequest request) =>
-        Save(request);
+    public void UpdateType(ProductsGroupUpdateTypeRequest request) => Save(request);
 }
