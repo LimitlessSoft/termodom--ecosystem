1c9e66a122c252ade50b6456caf0c9ee6a6519b7
Aleksa Ristic <aristiccitsira@gmail.com>
2024-09-12 02:39:29 +0200

    Stage

--- src/TD.Office/TD.Office.Public/TD.Office.Public.Api/Controllers/PartnersController.cs
+++ src/TD.Office/TD.Office.Public/TD.Office.Public.Api/Controllers/PartnersController.cs
@@ -1,8 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
+using TD.Komercijalno.Contracts.Requests.Partneri;
+using TD.Office.Public.Contracts.Interfaces.IManagers;
 
 namespace TD.Office.Public.Api.Controllers;
 
 [ApiController]
-public class PartnersController : ControllerBase
+public class PartnersController(ITDKomercijalnoApiManager komercijalnoApiManager)
+    : ControllerBase
 {
+    [HttpGet]
+    [Route("/partners")]
+    public IActionResult GetPartners([FromQuery] PartneriGetMultipleRequest request) =>
+        Ok(komercijalnoApiManager.GetPartnersAsync(request));
 }
