c359637eb93bd80a7705434dc96faae86a44ba2a
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-09-25 23:46:22 +0200

    Created controller for routes and implemented routes

--- src/TD.Office/TD.Office.Public/TD.Office.Public.Api.SpecifikacijaNovcaMicroservice/index.js
+++ src/TD.Office/TD.Office.Public/TD.Office.Public.Api.SpecifikacijaNovcaMicroservice/index.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
+const routes = require('./router')
 
-app.get('/specifikacija-novca', (req, res) => {
-    res.send('Hello World!');
-})
+app.use('/', routes)
 
 app.listen(port, () => {
-    console.log('Server started on port ' + port);
+    console.log('Server started on port ' + port)
 })
