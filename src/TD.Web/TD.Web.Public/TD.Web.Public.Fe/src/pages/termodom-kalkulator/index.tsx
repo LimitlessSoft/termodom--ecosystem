6a486e3917faed0cdb9273a6e5cd464a30284d19
Aleksa Ristic <aristiccitsira@gmail.com>
2024-09-30 19:13:44 +0200

    Stage kalkulator

---
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/pages/termodom-kalkulator/index.tsx
@@ -0,0 +1,110 @@
+import { Button, Grid, Paper, Typography } from '@mui/material'
+import { useState } from 'react'
+import { ArrowBack } from '@mui/icons-material'
+import {
+    TermodomKalkulatorFasada,
+    TermodomKalkulatorSuvaGradnja,
+} from '@/widgets'
+
+const Page = () => {
+    const [kalkulatorShown, setKalkulatorShown] = useState<string | undefined>(
+        undefined
+    )
+
+    return (
+        <Grid container p={2} justifyContent={`center`} gap={2}>
+            <Grid item sm={12}>
+                <Typography
+                    textAlign={`center`}
+                    component={`h1`}
+                    variant={`h4`}
+                    py={5}
+                >
+                    Termodom Kalkulator
+                </Typography>
+            </Grid>
+            <Grid item sm={12}>
+                {!kalkulatorShown && (
+                    <Grid
+                        container
+                        justifyContent={`center`}
+                        gap={2}
+                        textAlign={`center`}
+                    >
+                        <Grid item sm={12}>
+                            <Typography variant={`subtitle1`}>
+                                Izaberi tip kalkulacije
+                            </Typography>
+                        </Grid>
+                        <Grid item sm={4}>
+                            <Paper
+                                sx={{
+                                    p: 2,
+                                }}
+                            >
+                                <Button
+                                    onClick={() => {
+                                        setKalkulatorShown(`suva-gradnja`)
+                                    }}
+                                >
+                                    <Typography textAlign={`center`}>
+                                        Suva Gradnja
+                                    </Typography>
+                                </Button>
+                            </Paper>
+                        </Grid>
+                        <Grid item sm={4}>
+                            <Paper
+                                sx={{
+                                    p: 2,
+                                }}
+                            >
+                                <Button
+                                    onClick={() => {
+                                        setKalkulatorShown(`fasada`)
+                                    }}
+                                >
+                                    <Typography textAlign={`center`}>
+                                        Fasada
+                                    </Typography>
+                                </Button>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                )}
+                {kalkulatorShown && (
+                    <Grid
+                        container
+                        justifyContent={`center`}
+                        gap={2}
+                        maxWidth={`600px`}
+                        margin={`auto`}
+                    >
+                        <Grid item sm={12}>
+                            <Button
+                                startIcon={<ArrowBack />}
+                                onClick={() => {
+                                    setKalkulatorShown(undefined)
+                                }}
+                            >
+                                <Typography textAlign={`center`}>
+                                    Povratak
+                                </Typography>
+                            </Button>
+                        </Grid>
+                        <Grid item sm={12}>
+                            {kalkulatorShown === `suva-gradnja` && (
+                                <TermodomKalkulatorSuvaGradnja />
+                            )}
+                            {kalkulatorShown === `fasada` && (
+                                <TermodomKalkulatorFasada />
+                            )}
+                        </Grid>
+                    </Grid>
+                )}
+            </Grid>
+        </Grid>
+    )
+}
+
+export default Page
