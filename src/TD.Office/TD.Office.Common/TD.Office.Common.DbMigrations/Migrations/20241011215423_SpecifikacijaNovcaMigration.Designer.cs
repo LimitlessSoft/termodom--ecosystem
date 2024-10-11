398423e66e6c5564d6003b5623abb178c37e514e
Aleksa Ristic <aristiccitsira@gmail.com>
2024-10-11 23:57:53 +0200

    Specifikacija Novca Migration

--- src/TD.Office/TD.Office.Common/TD.Office.Common.DbMigrations/Migrations/20241011215423_SpecifikacijaNovcaMigration.Designer.cs
+++ src/TD.Office/TD.Office.Common/TD.Office.Common.DbMigrations/Migrations/20241011215423_SpecifikacijaNovcaMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TD.Office.Common.Repository;
@@ -11,9 +12,11 @@ using TD.Office.Common.Repository;
 namespace TD.Office.Common.DbMigrations.Migrations
 {
     [DbContext(typeof(OfficeDbContext))]
-    partial class OfficeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241011215423_SpecifikacijaNovcaMigration")]
+    partial class SpecifikacijaNovcaMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -321,7 +324,7 @@ namespace TD.Office.Common.DbMigrations.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("SpecifikacijaNovca", (string)null);
+                    b.ToTable("SpecifikacijeNovca");
                 });
 
             modelBuilder.Entity("TD.Office.Common.Contracts.Entities.UserEntity", b =>
