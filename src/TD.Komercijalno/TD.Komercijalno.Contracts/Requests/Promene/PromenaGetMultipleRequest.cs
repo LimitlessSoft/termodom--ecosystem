c50c01fde291099ee4330fa9179897984434e57c
Aleksa Ristic <aristiccitsira@gmail.com>
2024-10-22 23:00:15 +0200

    Implemented Promene controller with getmultiple endpoint

---
+++ src/TD.Komercijalno/TD.Komercijalno.Contracts/Requests/Promene/PromenaGetMultipleRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TD.Komercijalno.Contracts.Requests.Promene;
+
+public class PromenaGetMultipleRequest
+{
+    public string? KontoStartsWith { get; set; }
+    public int[]? PPID { get; set; }
+}
