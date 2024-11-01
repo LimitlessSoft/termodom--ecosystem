f658567bf3bd6555eb12ed40f0aabced2529afdf
Aleksa Milic <aleksamilickv@gmail.com>
2024-11-01 19:38:57 +0100

    hotfix

--- src/TD.Office/TD.Office.Public/TD.Office.Public.Contracts/Constants.cs
+++ src/TD.Office/TD.Office.Public/TD.Office.Public.Contracts/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string KomercijalnoApiUrlFormat = "https://localhost:7205"; //change to prod later TODO:
+        public const string KomercijalnoApiUrlFormat = "https://{0}-komercijalno.termodom.rs";
         public const int AzurirajCeneUslovFormiranjaMinWebOsnovaProductSuggestionSearchTextMinimumLength = 4;
         public const string PartnerIzvestajFinansijskoKomercijalnoLabelFormat = "TCMDZ {0}";
         public static readonly int[] DefaultPartnerIzvestajKomercijalnoDokumenti = { 10, 13, 14, 15, 22, 39, 40 };
