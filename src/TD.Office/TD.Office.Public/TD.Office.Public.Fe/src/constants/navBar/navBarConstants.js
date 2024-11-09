fcd82a2141742a16a6275eb95b2057adf2e499aa
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-11-09 21:23:49 +0100

    Created expanded left layout menu

---
+++ src/TD.Office/TD.Office.Public/TD.Office.Public.Fe/src/constants/navBar/navBarConstants.js
@@ -0,0 +1,13 @@
+export const NAV_BAR_CONSTANTS = {
+    MODULE_LABELS: {
+        PARTNERI: 'Partneri',
+        NALOG_ZA_PREVOZ: 'Nalog za prevoz',
+        PRORACUN: 'Proračun',
+        SPECIFIKACIJA_NOVCA: 'Specifikacija novca',
+        WEB_PRODAVNICA: 'Web prodavnica',
+        IZVESTAJI: 'Izveštaji',
+        KORISNICI: 'Korisnici',
+        KONTROLNA_TABLA: 'Kontrolna tabla',
+        ODJAVI_SE: 'Odjavi se',
+    },
+}
