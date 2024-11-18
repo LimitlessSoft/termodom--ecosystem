e3e37f1f1ea50f48a218ad86f4c8329e6b555825
Aleksa Milic <aleksamilickv@gmail.com>
2024-11-19 00:23:13 +0100

    Komercijalno i finansijsko po godinama entity and seed

---
+++ src/TD.Office/TD.Office.Common/TD.Office.Common.Contracts/Entities/KomercijalnoIFinansijskoPoGodinamaStatusEntity.cs
@@ -0,0 +1,8 @@
+﻿
+using LSCore.Contracts.Entities;
+
+namespace TD.Office.Common.Contracts.Entities;
+public class KomercijalnoIFinansijskoPoGodinamaStatusEntity : LSCoreEntity
+{
+    public string Naziv { get; set; }
+}
