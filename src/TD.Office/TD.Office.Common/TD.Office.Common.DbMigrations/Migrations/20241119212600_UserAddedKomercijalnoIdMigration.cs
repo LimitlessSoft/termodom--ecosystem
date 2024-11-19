9179568d372f96695dba243019f74aa8deee6c60
Aleksa Ristic <aristiccitsira@gmail.com>
2024-11-19 16:26:39 -0500

    Stage

---
+++ src/TD.Office/TD.Office.Common/TD.Office.Common.DbMigrations/Migrations/20241119212600_UserAddedKomercijalnoIdMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TD.Office.Common.DbMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserAddedKomercijalnoIdMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "KomercijalnoNalogId",
+                table: "Users",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KomercijalnoNalogId",
+                table: "Users");
+        }
+    }
+}
