55a1996b8b735d8991f2bcd240990488489ad267
Aleksa Ristic <aristiccitsira@gmail.com>
2024-11-30 08:29:14 -0500

    Moved discord bot to firebase function

--- src/TD.Automation/TD.Automation.Firebase/functions/discordOpenPullRequestsChecker.js
+++ src/TD.Automation/TD.Automation.Firebase/functions/discordOpenPullRequestsChecker.js
@@ -1,25 +1,23 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js')
+
+const discordToken = process.env.DISCORD_TOKEN
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 const privateTermodomEcosystemAnnouncementsChannel = '1312368860774535261'
 const roleId = '1312038212063203480' // termodom--ecosystem role
 const owner = 'LimitlessSoft'
 const repo = 'termodom--ecosystem'
 
 const fetchOpenPRs = async () => {
-    console.log(`Fetching PRs of ${owner}/${repo}`)
     const url = `https://api.github.com/repos/${owner}/${repo}/pulls?state=open`
 
-    console.log(`Setting headers with auth token ${process.env.GH_TOKEN}`)
-    // Replace 'your_personal_access_token' with your token if needed
     const headers = {
         Accept: 'application/vnd.github.v3+json',
-        // Uncomment the line below if using a personal access token
         Authorization: `token ${process.env.GH_TOKEN}`,
     }
 
     try {
-        console.log('Fetching')
         const response = await fetch(url, { headers })
 
-        console.log('Checking response')
         if (!response.ok) {
             throw new Error(
                 `Error: ${response.status} - ${response.statusText}`
@@ -31,31 +29,26 @@ const fetchOpenPRs = async () => {
     }
 }
 
-module.exports = {
-    checkForNewPRs: async function (discordClient) {
-        console.log('Checking for new PRs')
-
-        console.log('Getting channel')
-        const channel = discordClient.channels.cache.get(
+const discordOpenPullRequestsChecker = async () => {
+    const oncePromise = client.once(Events.ClientReady, async () => {
+        const channel = client.channels.cache.get(
             privateTermodomEcosystemAnnouncementsChannel
         )
 
         await fetchOpenPRs().then(async (prs) => {
-            console.log('PRs fetched')
             if (!prs) {
-                console.error('Failed to fetch PRs')
                 await channel.send('Failed to fetch PRs')
                 return
             }
             if (prs.length === 0) {
-                console.log('No open PRs')
                 await channel.send('No open PRs')
             } else {
-                console.log(`There are ${prs.length} open PRs`)
                 await channel.send(
                     `<@&${roleId}> There are ${prs.length} open PRs in the termodom--ecosystem repo. Check them out at https://github.com/${owner}/${repo}/pulls`
                 )
             }
         })
-    },
+    })
+    await client.login(discordToken)
 }
+module.exports = discordOpenPullRequestsChecker
---
+++ src/TD.Automation/TD.Automation.Firebase/functions/index.js
@@ -0,0 +1,12 @@
+const { onSchedule } = require('firebase-functions/v2/scheduler')
+const discordOpenPullRequestsChecker = require('./discordOpenPullRequestsChecker')
+const { defineSecret } = require('firebase-functions/params')
+const GH_TOKEN = defineSecret('GH_TOKEN')
+const DISCORD_TOKEN = defineSecret('DISCORD_TOKEN')
+
+exports.discordOpenPullRequestsChecker = onSchedule(
+    { secrets: [GH_TOKEN, DISCORD_TOKEN], schedule: '* 0/3 * * *' },
+    async (event) => {
+        await discordOpenPullRequestsChecker()
+    }
+)
