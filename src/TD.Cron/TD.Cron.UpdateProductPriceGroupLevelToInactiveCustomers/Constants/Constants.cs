742870a7c88fe6fc5886ab7b24af775f1d06b3e2
Aleksa Ristic <aristiccitsira@gmail.com>
2024-12-01 10:19:20 -0500

    Cleanup

--- src/TD.Cron/TD.Cron.UpdateProductPriceGroupLevelToInactiveCustomers/Constants/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TD.Cron.UpdateProductPriceGroupLevelToInactiveCustomers.Constants;
-public class Constants
-{
-    public static readonly string _inactiveStatusKey = "FEATURE_CRON_AZURIRAJ_NIVO_NEAKTIVNIM_KORISNICIMA";
-    public static readonly string _inactiveDaysKey = "AZURIRAJ_NIVO_NEAKTIVNIM_KORISNICIMA_NEAKTIVAN_JE_AKO_NIJE_KUPOVAO_DANA";
-    public static readonly string _updateLevelKey = "AZURIRAJ_NIVO_NEAKTIVNIM_KORISNICIMA_DESTINACIONI_NIVO";
-}
