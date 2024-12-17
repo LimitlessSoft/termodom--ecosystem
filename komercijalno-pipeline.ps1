564b17e194ab3ed67772cd763f130d5f433e4c3f
Aleksa Ristic <aristiccitsira@gmail.com>
2024-12-17 14:58:09 -0500

    Added develop env for komercijalno baze

--- komercijalno-pipeline.ps1
+++ komercijalno-pipeline.ps1
@@ -1,7 +1,9 @@
 $MainDir = Get-location
 
 docker stop termodom--komercijalno-api-TCMD-2024
+docker stop termodom--komercijalno-api-develop-TCMD-2024
 docker rm termodom--komercijalno-api-TCMD-2024
+docker rm termodom--komercijalno-api-develop-TCMD-2024
 
 docker stop termodom--komercijalno-api-TD-2024
 docker rm termodom--komercijalno-api-TD-2024
@@ -13,13 +15,16 @@ docker stop termodom--komercijalno-api-Magacin-2024
 docker rm termodom--komercijalno-api-Magacin-2024
 
 docker stop termodom--komercijalno-api-TCMD-2023
+docker stop termodom--komercijalno-api-develop-TCMD-2023
 docker rm termodom--komercijalno-api-TCMD-2023
+docker rm termodom--komercijalno-api-develop-TCMD-2023
 
 cd $MainDir/src/TD.Komercijalno/TD.Komercijalno.Api
 dotnet build
 dotnet publish -o obj/Docker/publish -c Release --runtime linux-x64 --self-contained False
 docker build -f ./Dockerfile -t limitlesssoft/termodom--komercijalno-api:$env:BUILD_NUMBER ./obj/Docker/publish
 docker run -p 32776:80 -e ConnectionString_Komercijalno=$env:ConnectionStrings_KomercijalnoFransiza2024TCMD --name termodom--komercijalno-api-TCMD-2024 -m 1G --restart=always -d limitlesssoft/termodom--komercijalno-api:$env:BUILD_NUMBER
+docker run -p 33776:80 -e ConnectionString_Komercijalno=$env:ConnectionStrings_Develop_KomercijalnoFransiza2024TCMD --name termodom--komercijalno-api-develop-TCMD-2024 -m 1G --restart=always -d limitlesssoft/termodom--komercijalno-api:$env:BUILD_NUMBER
 
 cd $MainDir/src/TD.Komercijalno/TD.Komercijalno.Api
 dotnet build
@@ -44,4 +49,4 @@ dotnet build
 dotnet publish -o obj/Docker/publish -c Release --runtime linux-x64 --self-contained False
 docker build -f ./Dockerfile -t limitlesssoft/termodom--komercijalno-api:$env:BUILD_NUMBER ./obj/Docker/publish
 docker run -p 32775:80 -e ConnectionString_Komercijalno=$env:ConnectionStrings_KomercijalnoFransiza2023TCMD --name termodom--komercijalno-api-TCMD-2023 -m 1G --restart=always -d limitlesssoft/termodom--komercijalno-api:$env:BUILD_NUMBER
-
+docker run -p 33775:80 -e ConnectionString_Komercijalno=$env:ConnectionStrings_Develop_KomercijalnoFransiza2023TCMD --name termodom--komercijalno-api-develop-TCMD-2023 -m 1G --restart=always -d limitlesssoft/termodom--komercijalno-api:$env:BUILD_NUMBER
