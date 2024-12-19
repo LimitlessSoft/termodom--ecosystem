843165d4f032a5cd4ccbeaf7f637658dfb0d907b
filipcvejic <l3g4l.05@gmail.com>
2024-12-19 01:15:50 +0100

    Refactored new partner dialog and simplified logic

--- src/TD.Office/TD.Office.Public/TD.Office.Public.Fe/src/widgets/Partneri/PartneriList/constants.ts
+++ src/TD.Office/TD.Office.Public/TD.Office.Public.Fe/src/widgets/Partneri/PartneriList/constants.ts
@@ -6,3 +6,20 @@ export const PARTNERI_PAGE_SIZE = 20
 export const PARTNERI_NEW_MESTA_PAYLOAD_DEFAULT_VALUE = undefined
 export const PARTNERI_NEW_KATEGORIJE_PAYLOAD_DEFAULT_VALUE = undefined
 export const PARTNERI_NEW_MIN_GROUPS_CHECKED = 4
+
+export const PARTNERI_NEW = {
+    VALIDATION_FIELDS: {
+        NAME: { LABEL: 'Naziv', FIELD: 'Naziv' },
+        ADDRESS: { LABEL: 'Adresa', FIELD: 'Adresa' },
+        POSTAL_CODE: { LABEL: 'Postanski broj', FIELD: 'Posta' },
+        CITY: { LABEL: 'Grad', FIELD: 'MestoId' },
+        PLACE: { LABEL: 'Mesto', FIELD: 'Mesto' },
+        EMAIL: { LABEL: 'Email', FIELD: 'Email' },
+        CONTACT: { LABEL: 'Kontakt', FIELD: 'Kontakt' },
+        JMBG: { LABEL: 'Maticni broj', FIELD: 'Mbroj' },
+        PIB: { LABEL: 'PIB', FIELD: 'Pib' },
+        MOBILE: { LABEL: 'Mobilni', FIELD: 'Mobilni' },
+        IN_PDV_SYSTEM: { LABEL: 'U PDV Sistemu', FIELD: 'UPdvSistemu' },
+        CATEGORY: { LABEL: 'Kategorija', FIELD: 'Kategorija' },
+    },
+}
