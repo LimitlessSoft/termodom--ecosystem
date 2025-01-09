bae40302c0ec9400741fc88190e04dcbe50a688d
Aleksa Ristic <aristiccitsira@gmail.com>
2025-01-09 19:23:10 +0100

    Added to pipeline

--- src/TD.Cron/TD.Cron.KomercijalnoDanasUpdate.CronJob/Program.cs
+++ src/TD.Cron/TD.Cron.KomercijalnoDanasUpdate.CronJob/Program.cs
@@ -7,23 +7,32 @@ var environment = TDKomercijalnoEnvironment.Development;
 var environment = TDKomercijalnoEnvironment.Production;
 #endif
 
+Console.WriteLine("Starting Komercijalno danas update...");
+Console.WriteLine($"Environment: {environment}");
 Dictionary<int, TDKomercijalnoClient> clients = new();
 
 var yearsInPast = 1;
+Console.WriteLine("Initializing clients...");
+Console.WriteLine("Years in past: " + yearsInPast);
 for(var i = DateTime.UtcNow.Year; i >= DateTime.UtcNow.Year - yearsInPast; i--)
     clients.Add(i, new TDKomercijalnoClient(i, environment));
 
-var currentTimeInBelgradeWithDaylightSaving = DateTime.UtcNow.AddHours(2);
+var currentTimeInBelgradeTimezone = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "Central European Standard Time");
+Console.WriteLine("Current time in Belgrade with daylight saving: " + currentTimeInBelgradeTimezone);
 
 var updateParametriRequest = new UpdateParametarRequest
 {
     Naziv = "danas",
-    Vrednost = currentTimeInBelgradeWithDaylightSaving.ToString("dd.MM.yyyy")
+    Vrednost = currentTimeInBelgradeTimezone.ToString("dd.MM.yyyy")
 };
 
+Console.WriteLine("Updating parametri...");
 var tasks =
     clients.Select(client =>
         client.Value.Parametri.UpdateAsync(updateParametriRequest))
         .ToList();
 
+Console.WriteLine("Waiting for all tasks to complete...");
 await Task.WhenAll(tasks);
+Console.WriteLine("All tasks completed.");
+Console.WriteLine("Komercijalno danas update finished.");
