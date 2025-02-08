87765a28ed026a5af043cc0d9125a91196cc435a
Aleksa Ristic <aristiccitsira@gmail.com>
2025-02-08 13:31:36 +0100

    Web Public adapted to vault

---
+++ src/TD.Web/TD.Web.Admin/TD.Web.Admin.Contracts/Vault/SecretsDto.cs
@@ -0,0 +1,19 @@
+namespace TD.Web.Admin.Contracts.Vault;
+
+public class SecretsDto
+{
+    public string DEPLOY_ENV { get; set; }
+    public string JWT_AUDIENCE { get; set; }
+    public string JWT_ISSUER { get; set; }
+    public string JWT_KEY { get; set; }
+    public string POSTGRES_HOST { get; set; }
+    public string POSTGRES_PASSWORD { get; set; }
+    public int POSTGRES_PORT { get; set; }
+    public string POSTGRES_USER { get; set; }
+    public int MINIO_PORT { get; set; }
+    public string MINIO_ACCESS_KEY { get; set; }
+    public string MINIO_SECRET_KEY { get; set; }
+    public string MINIO_HOST { get; set; }
+    [Obsolete("Should be removed in the future when upgraded to .NET 9 and uses new LSCore library")]
+    public string POSTGRES_DATABASE_NAME { get; set; }
+}
