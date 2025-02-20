7b8c2cdfbcc3cb43e19d74dac24d1a6ab26cbc85
Aleksa Ristic <aristiccitsira@gmail.com>
2025-02-20 13:46:03 +0100

    Stage

--- src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/widgets/Proizvodi/ProizvodiList/ui/OneTimePrice.tsx
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/widgets/Proizvodi/ProizvodiList/ui/OneTimePrice.tsx
@@ -20,7 +20,7 @@ export const OneTimePrice = (props: any) => {
             {minPrice == maxPrice ? (
                 <Typography>
                     {formatNumber(
-                        minPrice * (props.isWholesale ? 1 : 1 + props.vat / 100)
+                        maxPrice * (props.isWholesale ? 1 : 1 + props.vat / 100)
                     )}{' '}
                     RSD
                 </Typography>
@@ -50,7 +50,7 @@ export const OneTimePrice = (props: any) => {
                             variant={`subtitle2`}
                         >
                             {formatNumber(
-                                minPrice *
+                                maxPrice *
                                     (props.isWholesale
                                         ? 1
                                         : 1 + props.vat / 100)
