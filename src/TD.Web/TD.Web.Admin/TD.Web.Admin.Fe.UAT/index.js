e7b03e42d68c5c419149c8b9885f7046f897587e
filipcvejic <l3g4l.05@gmail.com>
2025-02-21 00:49:40 +0100

    Created vaultconfig.js

--- src/TD.Web/TD.Web.Admin/TD.Web.Admin.Fe.UAT/index.js
+++ src/TD.Web/TD.Web.Admin/TD.Web.Admin.Fe.UAT/index.js
@@ -2,12 +2,6 @@ import { promises as fs } from 'fs'
 import path from 'path'
 import { createDriver } from './driver.js'
 import { ENV } from './constants.js'
-//postgres package install
-
-//flow:
-//before test -
-//run test -
-//after test -
 
 const testsDir = path.resolve('./tests')
 
@@ -29,7 +23,7 @@ async function runTests() {
             throw new Error('Some tests failed')
         }
     } catch (err) {
-        console.error('Error running tests:', err)
+        console.error('Error running tests:', err.message)
         process.exit(1)
     }
 }
@@ -47,38 +41,51 @@ async function runTest(file) {
     const log = []
     const result = { passed: false }
 
-    if (typeof testModule.default.execution === 'function') {
-        log.push(`====================`)
-        let driver = await createDriver()
+    const { beforeExecution, afterExecution, execution } = testModule.default
 
-        if (typeof testModule.default.beforeExecution === 'function') {
-            testModule.default.beforeExecution()
-        }
-
-        try {
-            log.push(
-                `Starting test ${file} in browser: ${(
-                    await driver.getCapabilities()
-                ).get('browserName')}`
-            )
-            await testModule.default.execution(driver)
-            log.push(`Test ${file} finished successfully`)
-            console.log(log.join('\n'))
-            result.passed = true
-        } catch (err) {
-            log.push(`Test ${file} failed`)
-            log.push(err)
-            console.log(log.join('\n'))
-            result.passed = false
-        } finally {
-            await driver.quit()
-        }
-
-        if (typeof testModule.default.afterExecution === 'function') {
-            testModule.default.afterExecution()
-        }
-        return result
+    if (
+        (beforeExecution && !afterExecution) ||
+        (!beforeExecution && afterExecution)
+    ) {
+        throw new Error(
+            `Test module ${file} must define both 'beforeExecution' and 'afterExecution' if one of them exists.`
+        )
+    } else if (beforeExecution && typeof beforeExecution !== 'function') {
+        throw new Error(`'beforeExecution' in ${file} must be a function.`)
+    } else if (afterExecution && typeof afterExecution !== 'function') {
+        throw new Error(`'afterExecution' in ${file} must be a function.`)
+    } else if (!execution) {
+        throw new Error(`Test ${file} is missing an 'execution' function.`)
+    } else if (typeof execution !== 'function') {
+        throw new Error(`'execution' in ${file} must be a function.`)
     }
+
+    log.push(`====================`)
+    let driver = await createDriver()
+
+    try {
+        if (beforeExecution) await testModule.default.beforeExecution()
+
+        log.push(
+            `Starting test ${file} in browser: ${(
+                await driver.getCapabilities()
+            ).get('browserName')}`
+        )
+        await execution(driver)
+        log.push(`Test ${file} finished successfully`)
+        console.log(log.join('\n'))
+        result.passed = true
+    } catch (err) {
+        log.push(`Test ${file} failed`)
+        log.push(err)
+        console.log(log.join('\n'))
+        result.passed = false
+    } finally {
+        await driver.quit()
+        if (afterExecution) await afterExecution()
+    }
+
+    return result
 }
 
 function logTestResults(totalTests, passedTests) {
--- src/TD.Web/TD.Web.Public/TD.Web.Public.Fe.UAT/constants.js
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Fe.UAT/constants.js
@@ -1,6 +1,3 @@
-export const PROJECT_URL = process.env.PROJECT_URL || 'https://develop.termodom.rs/'
-export const BROWSER = process.env.BROWSER || 'firefox'
-export const ENV = process.env.ENV || 'github-action'
-export const SELENIUM_SERVER = process.env.SELENIUM_SERVER || 'selenium'
-
+export const PROJECT_URL =
+    process.env.PROJECT_URL || 'https://develop.termodom.rs/'
 export const WAIT_TIMEOUT = 20 * 1000
