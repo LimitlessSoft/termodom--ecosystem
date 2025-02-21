0bd26593e3001be81a517a1df3bad406143bbb3c
Aleksa Ristic <aristiccitsira@gmail.com>
2025-02-20 21:09:12 -0500

    Stage

--- src/TD.Web/TD.Web.Public/TD.Web.Public.Fe.UAT/vaultConfig.js
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Fe.UAT/vaultConfig.js
@@ -1,28 +1,57 @@
-import Vault from 'hashi-vault-js'
-
-const vault = new Vault({
-    https: true,
+const vault = {
     baseUrl: 'http://vault.termodom.rs:8199/v1',
-    timeout: 5000,
-    proxy: false,
-})
+    defaultSecret: 'office/public/fe',
+    defaultPath: 'office/public',
+    username: 'filip',
+    password: '123456789',
+    _token: null,
+    token: async () => {
+        if (!vault._token) {
+            await vault.login()
+        }
 
-function runOnVaultServer(func) {}
-
-async function initializeVault() {
-    try {
-        const status = await vault.healthCheck()
-        console.log('Vault Health Status:', status)
-
-        const token = (await vault.loginWithUserpass('filip', '123456789'))
-            .client_token
-
-        runOnVaultServer(vault.listKVSecrets)
-
-        vault.listKVSecrets(token).then((x) => console.log(x))
-    } catch (error) {
-        console.error('Error connecting to Vault:', error)
+        return vault._token
+    },
+    login: async () => {
+        const response = await fetch(vault.baseUrl + '/auth/userpass/login/' + vault.username, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: vault.username,
+                password: vault.password,
+            }),
+        })
+        const data = await response.json()
+        vault._token = data.auth.client_token
+    },
+    getSecret: async (path) => {
+        if (path && path[path.length - 1] === '/')
+            throw new Error('Secret should not end with /. Did you mean to use getSecrets?')
+        
+        const response = await fetch(vault.baseUrl + '/develop/data/' + (path || vault.defaultSecret), {
+            method: 'GET',
+            headers: {
+                'X-Vault-Token': await vault.token(),
+            },
+        })
+        const data = await response.json()
+        return data.data.data
+    },
+    getSecrets: async (path) => {
+        const response = await fetch(vault.baseUrl + '/develop/metadata/' + (path || vault.defaultPath), {
+            method: 'LIST',
+            headers: {
+                'X-Vault-Token': await vault.token(),
+            },
+        })
+        const data = await response.json()
+        return data.data.keys
     }
 }
 
-initializeVault()
+console.log(await vault.getSecret())
+console.log(await vault.getSecret('office/public/api'))
+console.log(await vault.getSecrets('/office/public/'))
+console.log(await vault.getSecrets('/office'))
