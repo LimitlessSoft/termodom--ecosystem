320aad1644f2327b33b66f91b97e1e0c7dd1fcb6
Aleksa Ristic <aristiccitsira@gmail.com>
2025-02-28 20:40:39 -0500

    Added csharpier

--- src/TD.Web/TD.Web.Public/TD.Web.Public.Api/Controllers/CalculatorController.cs
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Api/Controllers/CalculatorController.cs
@@ -7,13 +7,13 @@ namespace TD.Web.Public.Api.Controllers;
 [ApiController]
 public class CalculatorController(ICalculatorManager calculatorManager) : ControllerBase
 {
-    [HttpGet]
-    [Route("/calculator-items")]
-    public IActionResult GetCalculatorItems([FromQuery] GetCalculatorItemsRequest request) =>
-        Ok(calculatorManager.GetCalculatorItems(request));
+	[HttpGet]
+	[Route("/calculator-items")]
+	public IActionResult GetCalculatorItems([FromQuery] GetCalculatorItemsRequest request) =>
+		Ok(calculatorManager.GetCalculatorItems(request));
 
-    [HttpGet]
-    [Route("/calculator")]
-    public IActionResult GetCalculator([FromQuery] GetCalculatorRequest request) =>
-        Ok(calculatorManager.GetCalculator(request));
+	[HttpGet]
+	[Route("/calculator")]
+	public IActionResult GetCalculator([FromQuery] GetCalculatorRequest request) =>
+		Ok(calculatorManager.GetCalculator(request));
 }
