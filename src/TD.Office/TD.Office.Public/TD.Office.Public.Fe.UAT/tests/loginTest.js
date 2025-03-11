b2c6811a23593f8275693c8957000320c0d2f59f
filipcvejic <l3g4l.05@gmail.com>
2025-03-11 17:16:45 +0100

    CR Changes

--- src/TD.Office/TD.Office.Public/TD.Office.Public.Fe.UAT/tests/loginTest.js
+++ src/TD.Office/TD.Office.Public/TD.Office.Public.Fe.UAT/tests/loginTest.js
@@ -52,7 +52,7 @@ export default {
         )
         await loginButton.click()
 
-        await driver.sleep(2000)
+        await driver.sleep(2500)
 
         const firstNoteLabel = await (
             await driver.wait(
