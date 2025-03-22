1259b74be23509c516ab4b4fd09be52e825516af
Aleksa Ristic <aristiccitsira@gmail.com>
2025-03-22 11:06:23 -0400

    Upgrade LSCore to 9.1.x stage

--- src/TD.Web/TD.Web.Public/TD.Web.Public.Domain/Validators/Products/AddToCartRequestValidator.cs
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Domain/Validators/Products/AddToCartRequestValidator.cs
@@ -6,46 +6,45 @@ using TD.Web.Common.Contracts.Interfaces.IManagers;
 using TD.Web.Common.Repository;
 using TD.Web.Public.Contracts.Requests.Products;
 
-namespace TD.Web.Public.Domain.Validators.Products
+namespace TD.Web.Public.Domain.Validators.Products;
+
+public class AddToCartRequestValidator : LSCoreValidatorBase<AddToCartRequest>
 {
-	public class AddToCartRequestValidator : LSCoreValidatorBase<AddToCartRequest>
+	public AddToCartRequestValidator(IWebDbContextFactory dbContextFactory)
 	{
-		public AddToCartRequestValidator(IWebDbContextFactory dbContextFactory)
-		{
-			RuleFor(x => x.Id)
-				.Custom(
-					(id, context) =>
-					{
-						if (
-							!dbContextFactory
-								.Create<WebDbContext>()
-								.Products.Any(x => x.Id == id && x.IsActive)
-						)
-							context.AddFailure(OrderItemsValidationCodes.OIVC_001.GetDescription());
-					}
-				);
-
-			RuleFor(x => x.Quantity)
-				.NotEmpty()
-				.WithMessage(OrderItemsValidationCodes.OIVC_003.GetDescription());
-
-			RuleFor(x => x)
-				.Custom(
-					(request, context) =>
-					{
-						var product = dbContextFactory
+		RuleFor(x => x.Id)
+			.Custom(
+				(id, context) =>
+				{
+					if (
+						!dbContextFactory
 							.Create<WebDbContext>()
-							.Products.FirstOrDefault(x => x.IsActive && x.Id == request.Id);
-						if (product == null)
-							context.AddFailure(OrderItemsValidationCodes.OIVC_001.GetDescription());
+							.Products.Any(x => x.Id == id && x.IsActive)
+					)
+						context.AddFailure(OrderItemsValidationCodes.OIVC_001.GetDescription());
+				}
+			);
 
-						if (
-							product!.OneAlternatePackageEquals != null
-							&& request.Quantity % product!.OneAlternatePackageEquals != 0
-						)
-							context.AddFailure(OrderItemsValidationCodes.OIVC_003.GetDescription());
-					}
-				);
-		}
+		RuleFor(x => x.Quantity)
+			.NotEmpty()
+			.WithMessage(OrderItemsValidationCodes.OIVC_003.GetDescription());
+
+		RuleFor(x => x)
+			.Custom(
+				(request, context) =>
+				{
+					var product = dbContextFactory
+						.Create<WebDbContext>()
+						.Products.FirstOrDefault(x => x.IsActive && x.Id == request.Id);
+					if (product == null)
+						context.AddFailure(OrderItemsValidationCodes.OIVC_001.GetDescription());
+
+					if (
+						product!.OneAlternatePackageEquals != null
+						&& request.Quantity % product!.OneAlternatePackageEquals != 0
+					)
+						context.AddFailure(OrderItemsValidationCodes.OIVC_003.GetDescription());
+				}
+			);
 	}
 }
