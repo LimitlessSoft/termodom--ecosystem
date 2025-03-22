85cc82d5d8bb8669764d7ccd5750b9c378943cd8
Aleksa Ristic <aristiccitsira@gmail.com>
2025-03-22 07:27:30 -0400

    Products adding fixed

--- src/TD.Web/TD.Web.Admin/TD.Web.Admin.Domain/Managers/ProductManager.cs
+++ src/TD.Web/TD.Web.Admin/TD.Web.Admin.Domain/Managers/ProductManager.cs
@@ -19,225 +19,260 @@ using TD.Web.Common.Contracts.Interfaces.IRepositories;
 
 namespace TD.Web.Admin.Domain.Managers;
 
-public class ProductManager (
-    IHttpContextAccessor httpContextAccessor,
-    IProductRepository repository,
-    IProductPriceRepository productPriceRepository,
-    LSCoreContextUser contextUser,
-    IProductGroupRepository productGroupRepository,
-    ILogger<ProductManager> logger,
-    IUserManager userManager)
-    : IProductManager
+public class ProductManager(
+	IHttpContextAccessor httpContextAccessor,
+	IProductRepository repository,
+	IProductPriceRepository productPriceRepository,
+	LSCoreContextUser contextUser,
+	IProductGroupRepository productGroupRepository,
+	ILogger<ProductManager> logger,
+	IUserManager userManager
+) : IProductManager
 {
-    public ProductsGetDto Get(LSCoreIdRequest request)
-    {
-        var product = repository.GetMultiple()
-            .Where(x => x.Id == request.Id)
-            .Include(x => x.Groups)
-            .Include(x => x.Unit)
-            .Include(x => x.ProductPriceGroup)
-            .Include(x => x.Price)
-            .FirstOrDefault();
+	public ProductsGetDto Get(LSCoreIdRequest request)
+	{
+		var product = repository
+			.GetMultiple()
+			.Where(x => x.Id == request.Id)
+			.Include(x => x.Groups)
+			.Include(x => x.Unit)
+			.Include(x => x.ProductPriceGroup)
+			.Include(x => x.Price)
+			.FirstOrDefault();
 
-        if (product == null)
-            throw new LSCoreNotFoundException();
+		if (product == null)
+			throw new LSCoreNotFoundException();
 
-        var dto = product.ToDto<ProductEntity, ProductsGetDto>();
-        var userCanEditAll = contextUser.Id == 0 ||userManager.HasPermission(Permission.Admin_Products_EditAll);
-        dto.CanEdit = userCanEditAll || HasPermissionToEdit(product.Id);
-        return dto;
-    }
+		var dto = product.ToDto<ProductEntity, ProductsGetDto>();
+		var userCanEditAll =
+			contextUser.Id == 0 || userManager.HasPermission(Permission.Admin_Products_EditAll);
+		dto.CanEdit = userCanEditAll || HasPermissionToEdit(product.Id);
+		return dto;
+	}
 
-    public List<ProductsGetDto> GetMultiple(ProductsGetMultipleRequest request)
-    {
-        var query = repository.GetMultiple()
-            .Where(x =>
-                (string.IsNullOrWhiteSpace(request.SearchFilter) ||
-                 EF.Functions.ILike(x.Name, $"%{request.SearchFilter}%") ||
-                 EF.Functions.ILike(x.CatalogId, $"%{request.SearchFilter}%")) &&
-                (request.Id == null || request.Id.Length == 0 || request.Id.Contains(x.Id)) &&
-                (request.Status == null || request.Status.Length == 0 || request.Status.Contains(x.Status)) &&
-                (request.Groups == null || request.Groups.Length == 0 || request.Groups.Any(y => x.Groups.Any(z => z.Id == (int)y))) &&
-                (request.Classification == null || request.Classification.Length == 0 || request.Classification.Any(y => y == x.Classification)));
+	public List<ProductsGetDto> GetMultiple(ProductsGetMultipleRequest request)
+	{
+		var query = repository
+			.GetMultiple()
+			.Where(x =>
+				(
+					string.IsNullOrWhiteSpace(request.SearchFilter)
+					|| EF.Functions.ILike(x.Name, $"%{request.SearchFilter}%")
+					|| EF.Functions.ILike(x.CatalogId, $"%{request.SearchFilter}%")
+				)
+				&& (request.Id == null || request.Id.Length == 0 || request.Id.Contains(x.Id))
+				&& (
+					request.Status == null
+					|| request.Status.Length == 0
+					|| request.Status.Contains(x.Status)
+				)
+				&& (
+					request.Groups == null
+					|| request.Groups.Length == 0
+					|| request.Groups.Any(y => x.Groups.Any(z => z.Id == (int)y))
+				)
+				&& (
+					request.Classification == null
+					|| request.Classification.Length == 0
+					|| request.Classification.Any(y => y == x.Classification)
+				)
+			);
 
-        var products = query
-            .Include(x => x.Groups)
-            .ThenInclude(x => x.ManagingUsers)
-            .Include(x => x.Unit)
-            .Include(x => x.Price)
-            .ToList();
+		var products = query
+			.Include(x => x.Groups)
+			.ThenInclude(x => x.ManagingUsers)
+			.Include(x => x.Unit)
+			.Include(x => x.Price)
+			.ToList();
 
-        var dtoList = products.ToDtoList<ProductEntity, ProductsGetDto>();
-        var userCanEditAll = httpContextAccessor.HttpContext?.User?.Identity?.AuthenticationType == "ApiKey" || contextUser.Id == 0 || userManager.HasPermission(Permission.Admin_Products_EditAll);
+		var dtoList = products.ToDtoList<ProductEntity, ProductsGetDto>();
+		var userCanEditAll =
+			httpContextAccessor.HttpContext?.User?.Identity?.AuthenticationType == "ApiKey"
+			|| contextUser.Id == 0
+			|| userManager.HasPermission(Permission.Admin_Products_EditAll);
 
-        foreach (var dto in dtoList)
-        {
-            dto.CanEdit = userCanEditAll || HasPermissionToEdit(products.AsQueryable(), dto.Id);
+		foreach (var dto in dtoList)
+		{
+			dto.CanEdit = userCanEditAll || HasPermissionToEdit(products.AsQueryable(), dto.Id);
 
-            dto.PlatinumPriceWithoutVAT = PricesHelpers.CalculateProductPriceByLevel(
-                dto.MinWebBase,
-                dto.MaxWebBase,
-                3
-            );
-            dto.IronPriceWithoutVAT = PricesHelpers.CalculateProductPriceByLevel(
-                dto.MinWebBase,
-                dto.MaxWebBase,
-                0
-            );
-        }
+			dto.PlatinumPriceWithoutVAT = PricesHelpers.CalculateProductPriceByLevel(
+				dto.MinWebBase,
+				dto.MaxWebBase,
+				3
+			);
+			dto.IronPriceWithoutVAT = PricesHelpers.CalculateProductPriceByLevel(
+				dto.MinWebBase,
+				dto.MaxWebBase,
+				0
+			);
+		}
 
-        return dtoList.Where(x => x.CanEdit).ToList();
-    }
+		return dtoList.Where(x => x.CanEdit).ToList();
+	}
 
-    public List<ProductsGetDto> GetSearch(ProductsGetSearchRequest request) =>
-        repository.GetMultiple()
-            .Include(x => x.Groups)
-            .Include(x => x.Unit)
-            .Where(x =>
-                (request.Groups == null || request.Groups.Length == 0 || request.Groups.Any(y => x.Groups.Any(z => z.Id == (int)y))) &&
-                (request.Classification == null || request.Classification.Length == 0 || request.Classification.Any(y => y == x.Classification)) &&
-                (string.IsNullOrWhiteSpace(request.SearchTerm)) ||
-                EF.Functions.ILike(x.Name, $"%{request.SearchTerm}%") ||
-                EF.Functions.ILike(x.CatalogId, $"%{request.SearchTerm}%") ||
-                EF.Functions.ILike(x.Src, $"%{request.SearchTerm}%"))
-            .ToList()
-            .ToDtoList<ProductEntity, ProductsGetDto>();
+	public List<ProductsGetDto> GetSearch(ProductsGetSearchRequest request) =>
+		repository
+			.GetMultiple()
+			.Include(x => x.Groups)
+			.Include(x => x.Unit)
+			.Where(x =>
+				(
+					request.Groups == null
+					|| request.Groups.Length == 0
+					|| request.Groups.Any(y => x.Groups.Any(z => z.Id == (int)y))
+				)
+					&& (
+						request.Classification == null
+						|| request.Classification.Length == 0
+						|| request.Classification.Any(y => y == x.Classification)
+					)
+					&& (string.IsNullOrWhiteSpace(request.SearchTerm))
+				|| EF.Functions.ILike(x.Name, $"%{request.SearchTerm}%")
+				|| EF.Functions.ILike(x.CatalogId, $"%{request.SearchTerm}%")
+				|| EF.Functions.ILike(x.Src, $"%{request.SearchTerm}%")
+			)
+			.ToList()
+			.ToDtoList<ProductEntity, ProductsGetDto>();
 
-    public long Save(ProductsSaveRequest request)
-    {
-        if (string.IsNullOrWhiteSpace(request.Src))
-            request.Src = request.Name.GenerateSrc();
+	public long Save(ProductsSaveRequest request)
+	{
+		if (string.IsNullOrWhiteSpace(request.Src))
+			request.Src = request.Name.GenerateSrc();
 
-        var entity = request.Id == 0
-            ? new ProductEntity()
-            : repository.Get(request.Id!.Value);
+		var entity = request.Id is 0 or null
+			? new ProductEntity()
+			: repository.Get(request.Id!.Value);
 
-        entity.InjectFrom(request);
-        repository.UpdateOrInsert(entity);
+		entity.InjectFrom(request);
+		repository.UpdateOrInsert(entity);
 
-        #region Update product groups
+		#region Update product groups
 
-        // Get product entity since I need to include groups
-        var product = repository.GetMultiple()
-            .Include(x => x.Groups)
-            .First(x => x.Id == entity.Id);
+		// Get product entity since I need to include groups
+		var product = repository.GetMultiple().Include(x => x.Groups).First(x => x.Id == entity.Id);
 
-        var groups = productGroupRepository.GetMultiple()
-            .Include(x => x.ParentGroup)
-            .ToList();
-            
-        var groupToRemove = new List<long>();
-        foreach (var group in request.Groups)
-        {
-            var qGroup = groups.FirstOrDefault(x => x.Id == group && x.IsActive);
-            while(qGroup is { ParentGroup: not null })
-            {
-                if (request.Groups.Any(x => x == qGroup.ParentGroup.Id))
-                    groupToRemove.Add(qGroup.ParentGroup.Id);
-                qGroup = groups.FirstOrDefault(x => x.Id == qGroup.ParentGroup.Id && x.IsActive);
-            }
-        }
+		var groups = productGroupRepository.GetMultiple().Include(x => x.ParentGroup).ToList();
 
-        request.Groups.RemoveAll(x => groupToRemove.Contains(x));
-        product.Groups.Clear();
-        product.Groups.AddRange(groups.Where(x => request.Groups.Contains(x.Id)));
+		var groupToRemove = new List<long>();
+		foreach (var group in request.Groups)
+		{
+			var qGroup = groups.FirstOrDefault(x => x.Id == group && x.IsActive);
+			while (qGroup is { ParentGroup: not null })
+			{
+				if (request.Groups.Any(x => x == qGroup.ParentGroup.Id))
+					groupToRemove.Add(qGroup.ParentGroup.Id);
+				qGroup = groups.FirstOrDefault(x => x.Id == qGroup.ParentGroup.Id && x.IsActive);
+			}
+		}
 
-        repository.Update(product);
-        #endregion
+		request.Groups.RemoveAll(x => groupToRemove.Contains(x));
+		product.Groups.Clear();
+		product.Groups.AddRange(groups.Where(x => request.Groups.Contains(x.Id)));
 
-        return product.Id;
-    }
+		repository.Update(product);
+		#endregion
 
-    public List<LSCoreIdNamePairDto> GetClassifications() =>
-        Enum.GetValues(typeof(ProductClassification))
-            .Cast<ProductClassification>()
-            .Select(classification => new LSCoreIdNamePairDto
-            {
-                Id = (int)classification,
-                Name = classification.ToString()
-            })
-            .ToList();
+		return product.Id;
+	}
 
-    public void UpdateMaxWebOsnove(ProductsUpdateMaxWebOsnoveRequest request)
-    {
-        var productPrices = productPriceRepository.GetMultiple();
+	public List<LSCoreIdNamePairDto> GetClassifications() =>
+		Enum.GetValues(typeof(ProductClassification))
+			.Cast<ProductClassification>()
+			.Select(classification => new LSCoreIdNamePairDto
+			{
+				Id = (int)classification,
+				Name = classification.ToString()
+			})
+			.ToList();
 
-        foreach (var item in request.Items)
-        {
-            try
-            {
-                var productPrice = productPrices.FirstOrDefault(x => x.ProductId == item.ProductId) ?? new ProductPriceEntity()
-                {
-                    ProductId = item.ProductId,
-                    Min = item.MaxWebOsnova,
-                    Max = item.MaxWebOsnova
-                };
+	public void UpdateMaxWebOsnove(ProductsUpdateMaxWebOsnoveRequest request)
+	{
+		var productPrices = productPriceRepository.GetMultiple();
 
-                productPrice.Max = item.MaxWebOsnova;
-                productPriceRepository.UpdateOrInsert(productPrice);
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e.ToString());
-            }
-        }
-    }
+		foreach (var item in request.Items)
+		{
+			try
+			{
+				var productPrice =
+					productPrices.FirstOrDefault(x => x.ProductId == item.ProductId)
+					?? new ProductPriceEntity()
+					{
+						ProductId = item.ProductId,
+						Min = item.MaxWebOsnova,
+						Max = item.MaxWebOsnova
+					};
 
-    public void UpdateMinWebOsnove(ProductsUpdateMinWebOsnoveRequest request)
-    {
-        var productPrices = productPriceRepository.GetMultiple();
+				productPrice.Max = item.MaxWebOsnova;
+				productPriceRepository.UpdateOrInsert(productPrice);
+			}
+			catch (Exception e)
+			{
+				logger.LogError(e.ToString());
+			}
+		}
+	}
 
-        foreach (var item in request.Items)
-        {
-            var productPrice = productPrices.FirstOrDefault(x => x.ProductId == item.ProductId) ?? new ProductPriceEntity()
-            {
-                ProductId = item.ProductId,
-                Min = item.MinWebOsnova,
-                Max = item.MinWebOsnova
-            };
+	public void UpdateMinWebOsnove(ProductsUpdateMinWebOsnoveRequest request)
+	{
+		var productPrices = productPriceRepository.GetMultiple();
 
-            // If min price would be greater than max price, set min price to be same as max price
-            productPrice.Min = productPrice.Max < item.MinWebOsnova ? productPrice.Max : item.MinWebOsnova;
-            productPriceRepository.Update(productPrice);
-        }
-    }
+		foreach (var item in request.Items)
+		{
+			var productPrice =
+				productPrices.FirstOrDefault(x => x.ProductId == item.ProductId)
+				?? new ProductPriceEntity()
+				{
+					ProductId = item.ProductId,
+					Min = item.MinWebOsnova,
+					Max = item.MinWebOsnova
+				};
 
-    public bool HasPermissionToEdit(long productId) =>
-        HasPermissionToEdit(repository.GetMultiple().Include(x => x.Groups), productId);
+			// If min price would be greater than max price, set min price to be same as max price
+			productPrice.Min =
+				productPrice.Max < item.MinWebOsnova ? productPrice.Max : item.MinWebOsnova;
+			productPriceRepository.Update(productPrice);
+		}
+	}
 
-    /// <summary>
-    /// Checks if user has permission to edit product.
-    /// </summary>
-    /// <param name="products"></param>
-    /// <param name="productId"></param>
-    /// <returns></returns>
-    public bool HasPermissionToEdit(IQueryable<ProductEntity> products, long productId) =>
-        products.Where(x => x.IsActive
-                && x.Id == productId
-                && new List<ProductStatus>
-                {
-                    ProductStatus.AzuriranjeCekaOdobrenje,
-                    ProductStatus.NoviCekaOdobrenje,
-                    ProductStatus.AzuriranjeNaObradi,
-                    ProductStatus.AzuriranjeCekaOdobrenje
-                }.Contains(x.Status))
-            .SelectMany(x => x.Groups.SelectMany(y => y.ManagingUsers!.Select(z => z.Id)))
-            .Any(x => x == contextUser.Id!.Value);
+	public bool HasPermissionToEdit(long productId) =>
+		HasPermissionToEdit(repository.GetMultiple().Include(x => x.Groups), productId);
 
-    public void AppendSearchKeywords(CreateProductSearchKeywordRequest request)
-    {
-        request.Validate();
-        var product = repository.Get(request.Id);
+	/// <summary>
+	/// Checks if user has permission to edit product.
+	/// </summary>
+	/// <param name="products"></param>
+	/// <param name="productId"></param>
+	/// <returns></returns>
+	public bool HasPermissionToEdit(IQueryable<ProductEntity> products, long productId) =>
+		products
+			.Where(x =>
+				x.IsActive
+				&& x.Id == productId
+				&& new List<ProductStatus>
+				{
+					ProductStatus.AzuriranjeCekaOdobrenje,
+					ProductStatus.NoviCekaOdobrenje,
+					ProductStatus.AzuriranjeNaObradi,
+					ProductStatus.AzuriranjeCekaOdobrenje
+				}.Contains(x.Status)
+			)
+			.SelectMany(x => x.Groups.SelectMany(y => y.ManagingUsers!.Select(z => z.Id)))
+			.Any(x => x == contextUser.Id!.Value);
 
-        product.SearchKeywords ??= [];
-        product.SearchKeywords!.Add(request.Keyword.ToLower());
-        repository.Update(product);
-    }
+	public void AppendSearchKeywords(CreateProductSearchKeywordRequest request)
+	{
+		request.Validate();
+		var product = repository.Get(request.Id);
 
-    public void DeleteSearchKeywords(DeleteProductSearchKeywordRequest request)
-    {
-        var product = repository.Get(request.Id);
-        product.SearchKeywords?.RemoveAll(x => x.ToLower() == request.Keyword.ToLower());
-        repository.Update(product);
-    }
+		product.SearchKeywords ??= [];
+		product.SearchKeywords!.Add(request.Keyword.ToLower());
+		repository.Update(product);
+	}
+
+	public void DeleteSearchKeywords(DeleteProductSearchKeywordRequest request)
+	{
+		var product = repository.Get(request.Id);
+		product.SearchKeywords?.RemoveAll(x => x.ToLower() == request.Keyword.ToLower());
+		repository.Update(product);
+	}
 }
