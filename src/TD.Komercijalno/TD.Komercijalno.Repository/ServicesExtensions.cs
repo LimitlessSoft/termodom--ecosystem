33552475a33c632eede1d9b5a2b51e5c8c5d9df1
Aleksa Ristic <aristiccitsira@gmail.com>
2025-03-27 19:05:08 -0400

    Stage

--- src/TD.Komercijalno/TD.Komercijalno.Repository/ServicesExtensions.cs
+++ src/TD.Komercijalno/TD.Komercijalno.Repository/ServicesExtensions.cs
@@ -2,26 +2,25 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace TD.Komercijalno.Repository
+namespace TD.Komercijalno.Repository;
+
+public static class ServicesExtensions
 {
-    public static class ServicesExtensions
-    {
-        public static void RegisterDatabase(
-            this IServiceCollection serviceCollection,
-            IConfigurationRoot configurationRoot
-        )
-        {
-            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-            serviceCollection
-                .AddEntityFrameworkFirebird()
-                .AddDbContext<KomercijalnoDbContext>(
-                    (serviceProvider, options) =>
-                    {
-                        options
-                            .UseFirebird(configurationRoot["ConnectionString_Komercijalno"])
-                            .UseInternalServiceProvider(serviceProvider);
-                    }
-                );
-        }
-    }
+	public static void RegisterDatabase(
+		this IServiceCollection serviceCollection,
+		IConfigurationRoot configurationRoot
+	)
+	{
+		AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+		serviceCollection
+			.AddEntityFrameworkFirebird()
+			.AddDbContext<KomercijalnoDbContext>(
+				(serviceProvider, options) =>
+				{
+					options
+						.UseFirebird(configurationRoot["ConnectionString_Komercijalno"])
+						.UseInternalServiceProvider(serviceProvider);
+				}
+			);
+	}
 }
