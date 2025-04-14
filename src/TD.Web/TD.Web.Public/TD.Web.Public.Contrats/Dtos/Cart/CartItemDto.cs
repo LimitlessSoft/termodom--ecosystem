4036406907695ae0e05f006927996a56a77a8f91
Aleksa Ristic <aristiccitsira@gmail.com>
2025-04-14 18:50:31 -0400

    [TD.Web.Public.Api] Korpa > alt quantity and unit

--- src/TD.Web/TD.Web.Public/TD.Web.Public.Contrats/Dtos/Cart/CartItemDto.cs
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Contrats/Dtos/Cart/CartItemDto.cs
@@ -4,14 +4,16 @@ namespace TD.Web.Public.Contracts.Dtos.Cart;
 
 public class CartItemDto
 {
-    public long Id { get; set; }
-    public long ProductId { get; set; }
-    public string Name { get; set; }
-    public decimal Quantity { get; set; }
-    public string Unit { get; set; }
-    public decimal Price { get; set; }
-    public decimal VAT { get; set; }
-    public decimal PriceWithVAT { get => Price * ((VAT + 100) / 100); }
-    public decimal ValueWithVAT { get => PriceWithVAT * Quantity; }
-    public ProductStockType StockType { get; set; }
+	public long Id { get; set; }
+	public long ProductId { get; set; }
+	public string Name { get; set; }
+	public decimal Quantity { get; set; }
+	public string Unit { get; set; }
+	public decimal Price { get; set; }
+	public decimal? OneAlternatePackageEquals { get; set; }
+	public string? AlternateUnit { get; set; }
+	public decimal VAT { get; set; }
+	public decimal PriceWithVAT => Price * ((VAT + 100) / 100);
+	public decimal ValueWithVAT => PriceWithVAT * Quantity;
+	public ProductStockType StockType { get; set; }
 }
