653edc73e5a40af7f0b839c850659163166cd43f
filipcvejic <l3g4l.05@gmail.com>
2025-04-18 01:52:43 +0200

    Removed repetetive code

--- src/TD.Office/TD.Office.Public/TD.Office.Public.Fe/src/widgets/Korisnici/Singular/ui/KorisniciSingular.jsx
+++ src/TD.Office/TD.Office.Public/TD.Office.Public.Fe/src/widgets/Korisnici/Singular/ui/KorisniciSingular.jsx
@@ -6,32 +6,35 @@ import korisniciSingularFieldsConfig from '@/data/korisniciSingularFieldsConfig.
 export const KorisniciSingular = ({ user, onSaveUserData }) => (
     <Grid container p={2} maxWidth={520} gap={2}>
         {Object.entries(korisniciSingularFieldsConfig.FIELDS).map(
-            ([fieldKey, { KEY, LABEL, TYPE, VALIDATION, EDITABLE }]) => (
-                <KorisniciSingularDataField
-                    key={fieldKey}
-                    editable={EDITABLE}
-                    defaultValue={user[KEY] || ''}
-                    preLabel={`${LABEL}:`}
-                    type={TYPE}
-                    validation={VALIDATION}
-                    onSave={
-                        EDITABLE
-                            ? (value) =>
-                                  onSaveUserData(
-                                      fieldKey,
-                                      VALIDATION === 'integer' ? +value : value,
-                                      {
-                                          [KEY]:
-                                              VALIDATION === 'integer'
+            ([fieldKey, { KEY, LABEL, TYPE, VALIDATION, EDITABLE }]) => {
+                const isIntegerValidation = VALIDATION === 'integer'
+
+                return (
+                    <KorisniciSingularDataField
+                        key={fieldKey}
+                        editable={EDITABLE}
+                        defaultValue={user[KEY] || ''}
+                        preLabel={`${LABEL}:`}
+                        type={TYPE}
+                        validation={VALIDATION}
+                        onSave={
+                            EDITABLE
+                                ? (value) =>
+                                      onSaveUserData(
+                                          fieldKey,
+                                          isIntegerValidation ? +value : value,
+                                          {
+                                              [KEY]: isIntegerValidation
                                                   ? +value
                                                   : value,
-                                      },
-                                      LABEL
-                                  )
-                            : undefined
-                    }
-                />
-            )
+                                          },
+                                          LABEL
+                                      )
+                                : undefined
+                        }
+                    />
+                )
+            }
         )}
         <KorisniciSingularPermissions userId={user.id} />
     </Grid>
