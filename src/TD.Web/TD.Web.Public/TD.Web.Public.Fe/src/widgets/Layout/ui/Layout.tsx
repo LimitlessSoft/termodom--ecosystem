183d4db9b5b3b2e526917a099ef99790055aa1cb
Aleksa Ristic <aristiccitsira@gmail.com>
2025-04-19 02:38:06 -0400

    Stage

--- src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/widgets/Layout/ui/Layout.tsx
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/widgets/Layout/ui/Layout.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/widgets/Footer/ui/Footer'
 import { Header } from '@/widgets/Header'
 import { ReactNode } from 'react'
+import Head from 'next/head'
 
 interface ILayoutProps {
     children: ReactNode
@@ -10,10 +11,18 @@ export const Layout = (props: ILayoutProps): JSX.Element => {
     const { children } = props
 
     return (
-        <div className={`mainWrapper`}>
-            <Header />
-            <main style={{ position: 'relative' }}>{children}</main>
-            <Footer />
-        </div>
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                ></meta>
+            </Head>
+            <div className={`mainWrapper`}>
+                <Header />
+                <main style={{ position: 'relative' }}>{children}</main>
+                <Footer />
+            </div>
+        </>
     )
 }
