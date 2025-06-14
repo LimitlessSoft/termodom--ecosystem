975ecf0daad412c8c28a3436725ef48e802f210a
Aleksa Ristic <aristiccitsira@gmail.com>
2025-06-14 13:17:52 -0400

    Added posthog product search events

--- src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/widgets/Proizvodi/ProizvodiSearch/ui/ProizvodiSearch.tsx
+++ src/TD.Web/TD.Web.Public/TD.Web.Public.Fe/src/widgets/Proizvodi/ProizvodiSearch/ui/ProizvodiSearch.tsx
@@ -1,23 +1,23 @@
 import { mainTheme } from '@/app/theme'
 import { Replay, Search } from '@mui/icons-material'
-import {
-    Box,
-    Button,
-    Grid,
-    IconButton,
-    LinearProgress,
-    Paper,
-    Typography,
-} from '@mui/material'
+import { Button, Grid, IconButton, Paper, Typography } from '@mui/material'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { ProizvodiSearchInputBaseStyled } from './ProizvodiSearchInputBaseStyled'
+import posthog from 'posthog-js'
+import { POSTHOG_PRODUCT_SEARCH_EVENT } from '@/constants/posthogConstants'
 
 export const ProizvodiSearch = (props: any) => {
     const router = useRouter()
     const [searchValue, setSearchValue] = useState<string>('')
 
     const updateSearchQueryParameter = () => {
+        const text = searchValue?.trim()
+        if (text?.length > 0) {
+            posthog.capture(POSTHOG_PRODUCT_SEARCH_EVENT, {
+                text: searchValue?.trim(),
+            })
+        }
         router.push({
             pathname: router.asPath.split('?')[0],
             query: { ...router.query, pretraga: searchValue?.trim() },
@@ -48,6 +48,7 @@ export const ProizvodiSearch = (props: any) => {
                 }}
             >
                 <ProizvodiSearchInputBaseStyled
+                    id={`proizvodi-search-input`}
                     disabled={props.disabled}
                     sx={{ ml: 1, flex: 1 }}
                     value={searchValue}
