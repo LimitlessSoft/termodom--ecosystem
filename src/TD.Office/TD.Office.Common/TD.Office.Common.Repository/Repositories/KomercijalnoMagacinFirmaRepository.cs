9b5d5b0bcc50f5d1b1e8b9b767b5356c188f56f9
Aleksa Ristic <aristiccitsira@gmail.com>
2025-06-24 10:11:53 -0400

    Stage

--- src/TD.Office/TD.Office.Common/TD.Office.Common.Repository/Repositories/KomercijalnoMagacinFirmaRepository.cs
+++ src/TD.Office/TD.Office.Common/TD.Office.Common.Repository/Repositories/KomercijalnoMagacinFirmaRepository.cs
@@ -1,21 +1,29 @@
 using LSCore.Exceptions;
 using LSCore.Repository;
 using LSCore.Repository.Contracts;
+using Microsoft.Extensions.Logging;
 using TD.Office.Common.Contracts.Entities;
 using TD.Office.Common.Contracts.IRepositories;
 
 namespace TD.Office.Common.Repository.Repositories;
 
-public class KomercijalnoMagacinFirmaRepository(OfficeDbContext dbContext)
+public class KomercijalnoMagacinFirmaRepository(
+	OfficeDbContext dbContext,
+	ILogger<KomercijalnoMagacinFirmaRepository> logger
+)
 	: LSCoreRepositoryBase<KomercijalnoMagacinFirmaEntity>(dbContext),
 		IKomercijalnoMagacinFirmaRepository
 {
 	public KomercijalnoMagacinFirmaEntity GetByMagacinId(int polazniMagacinMagacinId)
 	{
 		var entity = GetBymagacinIdOrDefault(polazniMagacinMagacinId);
-		if (entity == null)
-			throw new LSCoreNotFoundException();
-		return entity;
+		if (entity != null)
+			return entity;
+		logger.LogError(
+			"Magacin sa ID {MagacinId} nije povezan sa firmom u office bazi tabela KomercijalnoMagacinFirma.",
+			polazniMagacinMagacinId
+		);
+		throw new LSCoreNotFoundException();
 	}
 
 	public KomercijalnoMagacinFirmaEntity? GetBymagacinIdOrDefault(int polazniMagacinMagacinId) =>
