c94447a5d3083faa6b86eed3942a9e6575e80221
Aleksa Ristic <aristiccitsira@gmail.com>
2025-06-26 19:50:35 -0400

    Hotfix

--- src/TD.Office/TD.Office.PregledIUplataPazara/TD.Office.PregledIUplataPazara.Api/Controllers/PregledIUplataPazaraController.cs
+++ src/TD.Office/TD.Office.PregledIUplataPazara/TD.Office.PregledIUplataPazara.Api/Controllers/PregledIUplataPazaraController.cs
@@ -5,22 +5,11 @@ using TD.Office.PregledIUplataPazara.Contracts.Responses;
 
 namespace TD.Office.PregledIUplataPazara.Api.Controllers;
 
-public class PregledIUplataPazaraController(
-	IPregledIUplataPazaraManager manager,
-	ILogger<PregledIUplataPazaraController> logger
-) : ControllerBase
+public class PregledIUplataPazaraController(IPregledIUplataPazaraManager manager) : ControllerBase
 {
 	[HttpGet]
 	[Route("/pregled-i-uplata-pazara")]
 	public async Task<IActionResult> GetPregledIUplataPazara(
 		[FromQuery] GetPregledIUplataPazaraRequest request
-	)
-	{
-		logger.LogInformation(
-			"Starting request for PregledIUplataPazara with parameters: {@Request}",
-			request
-		);
-		var res = await manager.GetAsync(request);
-		return Ok(res);
-	}
+	) => Ok(await manager.GetAsync(request));
 }
--- src/TD.Office/TD.Office.PregledIUplataPazara/TD.Office.PregledIUplataPazara.Domain/Managers/AuthKeyProvider.cs
+++ src/TD.Office/TD.Office.PregledIUplataPazara/TD.Office.PregledIUplataPazara.Domain/Managers/AuthKeyProvider.cs
@@ -4,16 +4,10 @@ using Microsoft.Extensions.Logging;
 
 namespace TD.Office.PregledIUplataPazara.Domain.Managers;
 
-public class AuthKeyProvider(IConfigurationRoot configurationRoot, ILogger<AuthKeyProvider> logger)
-	: ILSCoreAuthKeyProvider
+public class AuthKeyProvider(IConfigurationRoot configurationRoot) : ILSCoreAuthKeyProvider
 {
 	public bool IsValidKey(string key)
 	{
-		logger.LogInformation("Validating API key: {0}", key);
-		logger.LogInformation(
-			"Validating against configuration: {0}",
-			configurationRoot["API_KEYS"]
-		);
 #if DEBUG
 		return true;
 #else
