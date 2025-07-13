6479345a1a81a6922a252df0d2332f7ad2d95395
filipcvejic <l3g4l.05@gmail.com>
2025-07-13 23:09:13 +0200

    Implemented discord favicon test checker function

--- src/TD.Automation/TD.Automation.Firebase/functions/index.js
+++ src/TD.Automation/TD.Automation.Firebase/functions/index.js
@@ -1,6 +1,7 @@
 const { onSchedule } = require('firebase-functions/v2/scheduler')
 const discordOpenPullRequestsChecker = require('./discordOpenPullRequestsChecker')
 const { defineSecret } = require('firebase-functions/params')
+const discordFaviconTestChecker = require('./discordFaviconTestChecker')
 const GH_TOKEN = defineSecret('GH_TOKEN')
 const DISCORD_TOKEN = defineSecret('DISCORD_TOKEN')
 
@@ -23,5 +24,16 @@ exports.discordWeeklyTaskReviewsChecker = onSchedule(
     },
     async (event) => {
         await discordOpenPullRequestsChecker()
+    }
+)
+
+exports.discordFaviconTestChecker = onSchedule(
+    {
+        secrets: [GH_TOKEN, DISCORD_TOKEN],
+        schedule: '0 10 * * *',
+        timeZone: 'Europe/Belgrade',
+    },
+    async (_event) => {
+        await discordFaviconTestChecker()
     }
 )
