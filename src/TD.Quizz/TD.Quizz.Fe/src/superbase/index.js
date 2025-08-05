337ebecb5904e28a45125df3b96a0c501bf9bfe3
Aleksa Ristic <aristiccitsira@gmail.com>
2025-08-05 15:07:54 -0400

    qizz deployment integration

--- src/TD.Quizz/TD.Quizz.Fe/src/superbase/index.js
+++ src/TD.Quizz/TD.Quizz.Fe/src/superbase/index.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const projectUrl = "https://bziuicjjwweilryhhovs.supabase.co"
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6aXVpY2pqd3dlaWxyeWhob3ZzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MzUyNTc3NywiZXhwIjoyMDY5MTAxNzc3fQ.gUPjb3P9cwBR2DdjVl2FZNHIKd09INSYMFLh2pCnzvY"
+const projectUrl = process.env.SUPERBASE_URL
+const key = process.env.SUPERBASE_KEY
+const schema = process.env.SUPERBASE_SCHEMA
 const superbaseClient = createClient(projectUrl, key)
-export const superbaseSchema = superbaseClient.schema("public")
+export const superbaseSchema = superbaseClient.schema(schema)
