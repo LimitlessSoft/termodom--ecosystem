4c1010d2213e2ed88cef00f6418912116bbd5795
filipcvejic <l3g4l.05@gmail.com>
2025-09-06 14:06:01 +0200

    CR Changes

--- src/TD.Quizz/TD.Quizz.Fe/src/app/api/admin/quizzes/[quizzId]/assign-to-all/route.js
+++ src/TD.Quizz/TD.Quizz.Fe/src/app/api/admin/quizzes/[quizzId]/assign-to-all/route.js
@@ -2,12 +2,18 @@ import { quizzRepository } from '@/superbase/quizzRepository'
 import { NextResponse } from 'next/server'
 
 export async function POST(_request, { params }) {
-    const { quizzId } = await params
-    if (!quizzId)
-        return NextResponse.json(
-            { error: 'quizzId url param is required' },
-            { status: 400 }
-        )
+    try {
+        const { quizzId } = await params
+        if (!quizzId)
+            return NextResponse.json(
+                { error: 'quizzId url param is required' },
+                { status: 400 }
+            )
 
-    return NextResponse.json(await quizzRepository.assignToAllUsers(quizzId))
+        return NextResponse.json(
+            await quizzRepository.assignToAllUsers(quizzId)
+        )
+    } catch {
+        return NextResponse.json(null, { status: 500 })
+    }
 }
