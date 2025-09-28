2349892054257edb5cb891d0cb88365f21020715
Aleksa Ristic <aristiccitsira@gmail.com>
2025-09-28 21:27:33 +0200

    bugfix isTimeout

--- src/TD.Quizz/TD.Quizz.Fe/src/widgets/Quizz/ui/quizzQuestion.js
+++ src/TD.Quizz/TD.Quizz.Fe/src/widgets/Quizz/ui/quizzQuestion.js
@@ -45,7 +45,7 @@ export const QuizzQuestion = ({ question, onSuccessSubmit }) => {
         setIsSubmitting(true)
         if (
             isTimeout &&
-            isTimeout === false &&
+            isTimeout === true &&
             selectedAnswers.length === question.requiredAnswers
         ) {
             toast.info(
