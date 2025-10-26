bec5b56ca7fc6828bec2e1691682838f0c46818a
Aleksa Ristic <aristiccitsira@gmail.com>
2025-10-26 15:07:31 -0400

    Korpa additional dicsount message show condition

--- src/TD.Web/TD.Web.Admin/TD.Web.Admin.Contracts/Dtos/Orders/OrdersSummaryDto.cs
+++ src/TD.Web/TD.Web.Admin/TD.Web.Admin.Contracts/Dtos/Orders/OrdersSummaryDto.cs
@@ -1,7 +1,8 @@
-﻿namespace TD.Web.Admin.Contracts.Dtos.Orders
+﻿using TD.Web.Common.Contracts;
+namespace TD.Web.Admin.Contracts.Dtos.Orders
 {
-	public class OrdersSummaryDto
-	{
+	public class OrdersSummaryDto {
+		public bool AdditionalDiscountApplied => ValueWithoutVAT > LegacyConstants.MaximumCartValueForDiscount;
 		public decimal ValueWithoutVAT { get; set; }
 		public decimal VATValue { get; set; }
 		public decimal ValueWithVAT
