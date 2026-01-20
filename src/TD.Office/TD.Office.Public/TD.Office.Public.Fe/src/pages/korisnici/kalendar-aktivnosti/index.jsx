fc5407c20123b6ce7f03c946061bb34a6377198f
Aleksa Ristic <aristiccitsira@gmail.com>
2026-01-20 08:06:27 -0500

    Add Notes widget to kalendar aktivnosti

--- src/TD.Office/TD.Office.Public/TD.Office.Public.Fe/src/pages/korisnici/kalendar-aktivnosti/index.jsx
+++ src/TD.Office/TD.Office.Public/TD.Office.Public.Fe/src/pages/korisnici/kalendar-aktivnosti/index.jsx
@@ -1,10 +1,15 @@
-import { KalendarAktivnosti } from '@/widgets'
+import { KalendarAktivnosti, Notes } from '@/widgets'
 import { Grid } from '@mui/material'
 
 const KalendarAktivnostiPage = () => {
     return (
-        <Grid>
-            <KalendarAktivnosti />
+        <Grid container spacing={2}>
+            <Grid item xs={12} lg={9}>
+                <KalendarAktivnosti />
+            </Grid>
+            <Grid item xs={12} lg={3}>
+                <Notes />
+            </Grid>
         </Grid>
     )
 }
